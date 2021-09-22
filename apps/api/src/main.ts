17e0f147dec8def0cf91c88003fc5cbaffa5f017
Preslav Gerchev <preslav.gerchev@prodrive-technologies.com>
2021-09-22 11:26:22 +0200

    Add helmet config, add it to main.ts.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,13 @@
 
 import { Logger, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-
 import { AppModule } from './app/app.module'
 import { setupCors } from './config/cors.config'
 import { setupSwagger } from './config/swagger.config'
 import { setupExceptions } from './config/exceptions.config'
 import { setupValidation } from './config/validation.config'
 import { setupShutdownHooks } from './config/shutdown.config'
+import { setupHelmet } from './config/helmet.config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -21,6 +21,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix)
   app.enableVersioning({ type: VersioningType.URI })
 
+  setupHelmet(app)
   setupCors(app)
   setupSwagger(app)
   setupExceptions(app)
