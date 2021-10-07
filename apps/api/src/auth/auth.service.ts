f51d7e36eac9fda5b3ebbce2e27307735ad4e01c
Ilko Kacharov <kachar136@gmail.com>
2021-10-07 16:08:36 +0300

    Create person with keycloak id in local db

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -5,21 +5,30 @@ import { RequiredActionAlias } from '@keycloak/keycloak-admin-client/lib/defs/re
 
 import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
 export class AuthService {
-  constructor(private config: ConfigService, private admin: KeycloakAdminClient) {}
+  constructor(
+    private readonly config: ConfigService,
+    private readonly admin: KeycloakAdminClient,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  async issueToken(email: string, password: string): Promise<string> {
+    await this.admin.auth({
+      clientId: this.config.get<string>('keycloak.clientId') || '',
+      clientSecret: this.config.get<string>('keycloak.secret') || '',
+      grantType: 'password',
+      username: email,
+      password: password,
+    })
+    return this.admin.accessToken
+  }
 
   async login(loginDto: LoginDto) {
     try {
-      await this.admin.auth({
-        clientId: this.config.get<string>('keycloak.clientId') || '',
-        clientSecret: this.config.get<string>('keycloak.secret') || '',
-        grantType: 'password',
-        username: loginDto.email,
-        password: loginDto.password,
-      })
-      const jwt = this.admin.accessToken
+      const jwt = await this.issueToken(loginDto.email, loginDto.password)
       return { jwt }
     } catch (error) {
       const response = {
@@ -32,32 +41,12 @@ export class AuthService {
   }
 
   async createUser(registerDto: RegisterDto) {
-    console.log(this.admin)
-    console.log(this.config.get<string>('keycloak.clientId'))
     try {
-      await this.admin.auth({
-        grantType: 'client_credentials',
-        clientId: this.config.get<string>('keycloak.clientId') || '',
-        clientSecret: this.config.get<string>('keycloak.secret') || '',
-      })
-      return await this.admin.users.create({
-        username: registerDto.email,
-        email: registerDto.email,
-        firstName: registerDto.firstName,
-        lastName: registerDto.lastName,
-        enabled: true,
-        emailVerified: false,
-        groups: [],
-        requiredActions: [RequiredActionAlias.VERIFY_EMAIL],
-        attributes: {},
-        credentials: [
-          {
-            type: 'password',
-            value: registerDto.password,
-            temporary: false,
-          },
-        ],
-      })
+      await this.authenticateAdmin()
+      // Create user in Keycloak
+      const user = await this.createKeycloakUser(registerDto)
+      // Insert or connect person in app db
+      return await this.createPerson(registerDto, user.id)
     } catch (error) {
       const response = {
         error: error.message,
@@ -66,5 +55,47 @@ export class AuthService {
       console.error(response)
       return response
     }
+  }
+
+  private async authenticateAdmin() {
+    await this.admin.auth({
+      grantType: 'client_credentials',
+      clientId: this.config.get<string>('keycloak.clientId') || '',
+      clientSecret: this.config.get<string>('keycloak.secret') || '',
+    })
+  }
+
+  private async createKeycloakUser(registerDto: RegisterDto) {
+    return await this.admin.users.create({
+      username: registerDto.email,
+      email: registerDto.email,
+      firstName: registerDto.firstName,
+      lastName: registerDto.lastName,
+      enabled: true,
+      emailVerified: true,
+      groups: [],
+      requiredActions: [RequiredActionAlias.VERIFY_EMAIL],
+      attributes: {},
+      credentials: [
+        {
+          type: 'password',
+          value: registerDto.password,
+          temporary: false,
+        },
+      ],
+    })
+  }
+
+  private async createPerson(registerDto: RegisterDto, id: string) {
+    return await this.prismaService.person.upsert({
+      create: {
+        id,
+        email: registerDto.email,
+        firstName: registerDto.firstName,
+        lastName: registerDto.lastName,
+      },
+      update: {},
+      where: { email: registerDto.email },
+    })
   }
 }
