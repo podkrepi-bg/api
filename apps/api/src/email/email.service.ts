32f71d1506b9cb0f073bacc94b1bb2f89d87bf1c
Ilko Kacharov <kachar136@gmail.com>
2021-10-25 18:21:48 +0300

    Disable sending emails when sendgrid key is not provided

--- apps/api/src/email/email.service.ts
+++ apps/api/src/email/email.service.ts
@@ -6,16 +6,21 @@ import { Email } from './email.interface'
 
 @Injectable()
 export class EmailService {
+  private enabled = true
+
   constructor(private config: ConfigService) {
     const apiKey = config.get<string>('sendgrid.apiKey')
     if (apiKey) {
       sgMail.setApiKey(apiKey)
     } else {
       Logger.warn('no apiKey for sendgrid, will not send emails')
+      this.enabled = false
     }
   }
 
-  async send(email: Email) {
+  async send(email: Email): Promise<void> {
+    if (!this.enabled) return
+
     try {
       await sgMail.send(email)
     } catch (err) {
