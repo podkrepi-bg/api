c8905e79ff0ac02ba573fc39ce19f7e0cdcebf60
quantum-grit <quantum-grit@gmail.com>
2022-02-02 18:42:51 +0200

    deleted .history folder

--- .history/apps/api/src/bootcamp/bootcamp.module_20220122145103.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { BootcampService } from './bootcamp.service'
-import { BootcampController } from './bootcamp.controller'
-
-@Module({
-  controllers: [BootcampController],
-  providers: [BootcampService],
-})
-export class BootcampModule {}
