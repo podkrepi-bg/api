f9a895350b29b1b31f1bfa7cbce035d11dce4330
Ilko Kacharov <kachar136@gmail.com>
2022-02-03 16:56:07 +0200

    Revert "country crud initial"
    This reverts commit 586a64c17d4b9274168c52f0c04a3f6b65405e95.

--- apps/api/src/country/country.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { CountryService } from './country.service'
-import { CountryController } from './country.controller'
-
-@Module({
-  controllers: [CountryController],
-  providers: [CountryService],
-})
-export class CountryModule {}
--- apps/api/src/country/dto/update-country.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger'
-import { CreateCountryDto } from './create-country.dto'
-
-export class UpdateCountryDto extends PartialType(CreateCountryDto) {}
