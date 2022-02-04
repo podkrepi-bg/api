47cbbc8b7ee0dfc891f1a3b581a0597d5d9b1d1f
dplamenov <dimitar.plamenov@gmail.com>
2022-02-04 20:48:55 +0200

    return all beneficiaries and all campaigns of coordinator

--- apps/api/src/coordinator/coordinator.service.ts
+++ apps/api/src/coordinator/coordinator.service.ts
@@ -12,11 +12,11 @@ export class CoordinatorService {
   }
 
   findAll() {
-    return this.prisma.coordinator.findMany({ include: { person: true } })
+    return this.prisma.coordinator.findMany({ include: { person: true, beneficiaries: true, campaigns: true } })
   }
 
   findOne(id: string) {
-    return this.prisma.coordinator.findUnique({ where: { id }, include: { person: true } })
+    return this.prisma.coordinator.findUnique({ where: { id }, include: { person: true, beneficiaries: true, campaigns: true } })
   }
 
   update(id: string, updateCoordinatorDto: UpdateCoordinatorDto) {
