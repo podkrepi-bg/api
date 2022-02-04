5f40d0140492a29824e4c5a2f3231b6fc045b1a4
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-04 14:12:19 +0200

    country dto

--- apps/api/src/country/dto/create-country.dto.ts
+++ apps/api/src/country/dto/create-country.dto.ts
@@ -1,1 +1,22 @@
-export class CreateCountryDto {}
+import { ApiProperty } from '@nestjs/swagger'
+import { City } from '@prisma/client'
+import { Expose } from 'class-transformer'
+import { IsOptional, IsString } from 'class-validator'
+
+@Expose()
+export class CreateCountryDto {
+  @ApiProperty()
+  @Expose()
+  @IsString()
+  name: string
+
+  @ApiProperty()
+  @Expose()
+  @IsString()
+  countryCode: string
+
+  @ApiProperty()
+  @Expose()
+  @IsOptional()
+  cities: City[]
+}
