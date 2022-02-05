2c0e861ea10019e341861a3c587346ebe1d9a77d
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-02-05 11:08:40 +0200

    add expense module to app module , import PrismaService to providers of expense resource and create all the endpoints public

--- apps/api/src/expenses/expenses.controller.ts
+++ apps/api/src/expenses/expenses.controller.ts
@@ -1,32 +1,39 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Public } from 'nest-keycloak-connect';
+
 import { ExpensesService } from './expenses.service';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
 
 @Controller('expenses')
 export class ExpensesController {
-  constructor(private readonly expensesService: ExpensesService) {}
+  constructor(private readonly expensesService: ExpensesService) { }
 
+  @Public()
   @Post()
   create(@Body() createExpenseDto: CreateExpenseDto) {
     return this.expensesService.create(createExpenseDto);
   }
 
+  @Public()
   @Get()
   findAll() {
     return this.expensesService.findAll();
   }
 
+  @Public()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.expensesService.findOne(+id);
   }
 
+  @Public()
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateExpenseDto: UpdateExpenseDto) {
     return this.expensesService.update(+id, updateExpenseDto);
   }
 
+  @Public()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.expensesService.remove(+id);
--- apps/api/src/expenses/expenses.module.ts
+++ apps/api/src/expenses/expenses.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
 import { ExpensesController } from './expenses.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [ExpensesController],
-  providers: [ExpensesService]
+  providers: [ExpensesService, PrismaService]
 })
-export class ExpensesModule {}
+export class ExpensesModule { }
