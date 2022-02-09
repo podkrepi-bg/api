2f65b810465a964f365497a424c06f0f1e9f4b18
PepiPetrov <pepi_petrov@outlook.com>
2022-02-09 17:51:38 +0200

    commit

---
+++ apps/api/src/campaign-types/campaign-types.controller.ts
@@ -0,0 +1,58 @@
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Public } from 'nest-keycloak-connect';
+import { CampaignTypesService } from './campaign-types.service';
+
+@Controller('campaign-types')
+export class CampaignTypesController {
+  constructor(private readonly campaignTypesService: CampaignTypesService) { }
+
+  @Post('add')
+  @Public()
+  async create(@Body() createCampaignTypeDto: {
+    name: string;
+    slug: string;
+    description?: string;
+  }) {
+    return await this.campaignTypesService.create(createCampaignTypeDto);
+  }
+
+  @Get('list')
+  @Public()
+  async findAll() {
+    return await this.campaignTypesService.findAll();
+  }
+
+  @Get('view/:id')
+  @Public()
+  async findOne(@Param('id') id: string) {
+    return await this.campaignTypesService.findOne(id);
+  }
+
+  @Put('edit/:id')
+  @Public()
+  async update(@Param('id') id: string, @Body() updateCampaignTypeDto: {
+    name: string;
+    slug: string;
+    description?: string;
+  }) {
+    return await this.campaignTypesService.update(id, updateCampaignTypeDto);
+  }
+
+  @Delete('remove/:id')
+  @Public()
+  async remove(@Param('id') id: string) {
+    return await this.campaignTypesService.remove(id);
+  }
+
+  @Get('/search/name/:key')
+  @Public()
+  async searchByName(@Param("key") keyword: string) {
+    return await this.campaignTypesService.searchByCategory(keyword)
+  }
+
+  @Get('/search/category/:key')
+  @Public()
+  async searchByCategory(@Param("key") keyword: string) {
+    return await this.campaignTypesService.searchByCategory(keyword)
+  }
+}
