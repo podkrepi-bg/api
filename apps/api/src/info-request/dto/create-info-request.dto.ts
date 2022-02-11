40a3c1393e80b94a9055a87d74f6a91a033fe589
dplamenov <dimitar.plamenov@gmail.com>
2022-02-11 07:13:00 +0200

    personId must be UUID

--- apps/api/src/info-request/dto/create-info-request.dto.ts
+++ apps/api/src/info-request/dto/create-info-request.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Expose } from 'class-transformer'
-import { IsString } from 'class-validator'
+import { IsString, IsUUID } from 'class-validator'
 
 export class CreateInfoRequestDto {
     @ApiProperty()
     @Expose()
     @IsString()
+    @IsUUID()
     personId: string;
     @ApiProperty()
     @Expose()
