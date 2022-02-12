d94360173eeb2dd88b5cfd2e9a25b74e4447626d
dplamenov <dimitar.plamenov@gmail.com>
2022-02-12 11:11:58 +0200

    fix
    fix

--- apps/api/src/info-request/info-request.module.ts
+++ apps/api/src/info-request/info-request.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { InfoRequestService } from './info-request.service';
-import { InfoRequestController } from './info-request.controller';
-import { PrismaService } from '../prisma/prisma.service';
+import { Module } from '@nestjs/common'
+import { InfoRequestService } from './info-request.service'
+import { InfoRequestController } from './info-request.controller'
+import { PrismaService } from '../prisma/prisma.service'
+import { SupportService } from '../support/support.service'
+import { EmailService } from '../email/email.service'
+import { TemplateService } from '../email/template.service'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
   controllers: [InfoRequestController],
-  providers: [InfoRequestService, PrismaService]
+  providers: [
+    InfoRequestService,
+    PrismaService,
+    SupportService,
+    EmailService,
+    TemplateService,
+    ConfigService,
+  ],
 })
 export class InfoRequestModule {}
