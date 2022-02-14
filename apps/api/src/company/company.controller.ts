ae1a2127c61a49dd3da0749cc80c4c0062d9b0fd
Todor Belchev <todorbelchev@live.com>
2022-02-14 12:57:25 +0200

    Extracted keycloak token interface

--- apps/api/src/company/company.controller.ts
+++ apps/api/src/company/company.controller.ts
@@ -10,40 +10,11 @@ import {
 } from '@nestjs/common'
 import { AuthenticatedUser } from 'nest-keycloak-connect'
 
+import { KeycloakTokenParsed } from '../auth/keycloak'
+
 import { CompanyService } from './company.service'
 import { CreateCompanyDto } from './dto/create-company.dto'
 import { UpdateCompanyDto } from './dto/update-company.dto'
-
-interface KeycloakTokenParsed extends KeycloakProfile {
-  exp?: number
-  iat?: number
-  auth_time?: number
-  jti?: string
-  iss?: string
-  sub?: string
-  typ?: string
-  azp?: string
-  acr?: string
-  session_state?: string
-  'allowed-origins': string[]
-  realm_access?: KeycloakRoles
-  resource_access?: KeycloakResourceAccess
-  scope?: string
-}
-interface KeycloakResourceAccess {
-  [key: string]: KeycloakRoles
-}
-interface KeycloakRoles {
-  roles: string[]
-}
-interface KeycloakProfile {
-  name?: string
-  given_name?: string
-  family_name?: string
-  email?: string
-  email_verified?: string
-  preferred_username?: string
-}
 
 @Controller('company')
 export class CompanyController {
