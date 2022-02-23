b1346aa89f825ede7d0c92d2d3004441f093e6df
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-02-23 14:51:55 +0200

    added prisma service to service test file

--- apps/api/src/vault/vault.service.spec.ts
+++ apps/api/src/vault/vault.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from '../prisma/prisma.service'
 import { VaultService } from './vault.service'
 
 describe('VaultService', () => {
@@ -6,7 +7,7 @@ describe('VaultService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VaultService],
+      providers: [VaultService, PrismaService],
     }).compile()
 
     service = module.get<VaultService>(VaultService)
