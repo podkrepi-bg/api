8ebedbdacc7c5d13bcda065cd467eb8ee73f2adc
PepiPetrov <pepi_petrov@outlook.com>
2022-02-24 19:21:38 +0200

    fixed tests

--- apps/api/src/person/person.controller.spec.ts
+++ apps/api/src/person/person.controller.spec.ts
@@ -1,20 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PersonController } from './person.controller';
-import { PersonService } from './person.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from '../prisma/prisma.service'
+import { PersonController } from './person.controller'
+import { PersonService } from './person.service'
 
 describe('PersonController', () => {
-  let controller: PersonController;
+  let controller: PersonController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PersonController],
-      providers: [PersonService],
-    }).compile();
+      providers: [PersonService, PrismaService],
+    }).compile()
 
-    controller = module.get<PersonController>(PersonController);
-  });
+    controller = module.get<PersonController>(PersonController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
