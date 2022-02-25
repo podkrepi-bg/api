70a0d13673775e5062e954c601ac2da7cd3298e8
PepiPetrov <pepi_petrov@outlook.com>
2022-02-25 11:51:10 +0200

    fixed tests

--- apps/api/src/campaign-types/campaign-types.service.spec.ts
+++ apps/api/src/campaign-types/campaign-types.service.spec.ts
@@ -1,18 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CampaignTypesService } from './campaign-types.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from '../prisma/prisma.service'
+import { CampaignTypesService } from './campaign-types.service'
 
 describe('CampaignTypesService', () => {
-  let service: CampaignTypesService;
+  let service: CampaignTypesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CampaignTypesService],
-    }).compile();
+      providers: [CampaignTypesService, PrismaService],
+    }).compile()
 
-    service = module.get<CampaignTypesService>(CampaignTypesService);
-  });
+    service = module.get<CampaignTypesService>(CampaignTypesService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
