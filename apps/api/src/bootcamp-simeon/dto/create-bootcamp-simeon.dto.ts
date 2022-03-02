7451edf0404b83df298a9125b7e932b22871e0c1
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-03-02 16:48:13 +0200

    Started writing the DTO

--- apps/api/src/bootcamp-simeon/dto/create-bootcamp-simeon.dto.ts
+++ apps/api/src/bootcamp-simeon/dto/create-bootcamp-simeon.dto.ts
@@ -1,1 +1,24 @@
-export class CreateBootcampSimeonDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateBootcampSimeonDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsOptional()
+  phoneNumber: number;
+}
