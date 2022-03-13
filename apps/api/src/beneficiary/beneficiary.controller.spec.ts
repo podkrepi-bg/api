415e2c6e94f96f398d5971ffe66e4714149f11ad
PepiPetrov <pepi_petrov@outlook.com>
2022-03-13 15:54:20 +0200

    fixed create campaign type dto

--- apps/api/src/beneficiary/beneficiary.controller.spec.ts
+++ apps/api/src/beneficiary/beneficiary.controller.spec.ts
@@ -5,6 +5,7 @@ import { BeneficiaryService } from './beneficiary.service'
 
 describe('BeneficiaryController', () => {
   let controller: BeneficiaryController
+  // let service: BeneficiaryService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +14,15 @@ describe('BeneficiaryController', () => {
     }).compile()
 
     controller = module.get<BeneficiaryController>(BeneficiaryController)
+    // service = module.get<BeneficiaryService>(BeneficiaryService)
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
   })
+
+  // it('should return list of beneficiaries', () => {
+  //   const result = []
+  //   jest.spyOn(service, 'findAll').mockImplementation(() => result)
+  // })
 })
--- apps/api/src/campaign-types/dto/create-campaign-type.dto.ts
+++ apps/api/src/campaign-types/dto/create-campaign-type.dto.ts
@@ -1,5 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { Expose } from 'class-transformer'
+import { IsOptional, IsString, IsUUID } from 'class-validator'
+
+@Expose()
 export class CreateCampaignTypeDto {
+  @ApiProperty()
+  @IsString()
   name: string
+  @ApiProperty()
   slug: string
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
   description?: string
+  @ApiProperty()
+  @IsUUID()
+  @IsOptional()
+  parentId?: string
 }
