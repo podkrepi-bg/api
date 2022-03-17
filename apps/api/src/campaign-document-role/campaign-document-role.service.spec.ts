6b527b8c85108f350f630879fefe43778fbb90b0
borislavstoychev <stoy4ew@gmail.com>
2022-03-17 20:51:18 +0200

    service tests

--- apps/api/src/campaign-document-role/campaign-document-role.service.spec.ts
+++ apps/api/src/campaign-document-role/campaign-document-role.service.spec.ts
@@ -1,10 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { prismaMock } from '../prisma/prisma-client.mock'
 import { PrismaService } from '../prisma/prisma.service'
 import { CampaignDocumentRoleService } from './campaign-document-role.service'
 
+const mockData = [
+  {
+    id: '5f9221cf-f067-4011-8d2a-66b59509fb7b',
+    name: 'Bobby',
+    description: '',
+  },
+  {
+    id: 'dc7c8d51-732a-4b82-a87e-f62a9c2e9a68',
+    name: 'image',
+    description: '',
+  },
+  {
+    id: 'dc7c8d51-732a-4b82-a87e-f62a9c2e9a99',
+    name: 'image',
+    description: '',
+  },
+]
+
 describe('CampaignDocumentRoleService', () => {
   let service: CampaignDocumentRoleService
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CampaignDocumentRoleService, PrismaService],
@@ -16,4 +34,8 @@ describe('CampaignDocumentRoleService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined()
   })
+  // it('should return all documents', async () => {
+  //   prismaMock.campaignDocumentRole.findMany.mockResolvedValue(mockData)
+  //   await expect(service.findAll()).resolves.toEqual(mockData)
+  // })
 })
