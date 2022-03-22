53c52c219d6e2c06968e8009d9b1a1b10e02c83a
borislavstoychev <stoy4ew@gmail.com>
2022-03-22 20:03:03 +0200

    tests Using a real database

--- apps/api/src/campaign-document-role/campaign-document-role.service.spec.ts
+++ apps/api/src/campaign-document-role/campaign-document-role.service.spec.ts
@@ -1,103 +1,89 @@
 import { NotFoundException } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
-import { CampaignDocumentRole, PrismaPromise } from '@prisma/client'
 import { isUUID } from 'class-validator'
-import { prismaMock } from '../prisma/prisma-client.mock'
 import { PrismaService } from '../prisma/prisma.service'
 import { CampaignDocumentRoleService } from './campaign-document-role.service'
 
-const mockData = [
-  {
-    id: '5f9221cf-f067-4011-8d2a-66b59509fb7b',
-    createdAt: new Date('2000-10-31T01:30:00.000-05:00'),
-    updatedAt: new Date('2000-10-31T01:30:00.000-05:00'),
-    name: 'Bobby',
-    description: '',
-  },
-  {
-    id: 'dc7c8d51-732a-4b82-a87e-f62a9c2e9a68',
-    createdAt: new Date('2000-10-31T01:30:00.000-05:00'),
-    updatedAt: new Date('2000-10-31T01:30:00.000-05:00'),
-    name: 'image',
-    description: '',
-  },
-  {
-    id: 'dc7c8d51-732a-4b82-a87e-f62a9c2e9a99',
-    createdAt: new Date('2000-10-31T01:30:00.000-05:00'),
-    updatedAt: new Date('2000-10-31T01:30:00.000-05:00'),
-    name: 'image',
-    description: '',
-  },
-]
-
 describe('CampaignDocumentRoleService', () => {
   let service: CampaignDocumentRoleService
-  const prismaService = prismaMock
+  let prismaService: PrismaService
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        CampaignDocumentRoleService,
-        {
-          provide: PrismaService,
-          useValue: prismaMock,
-        },
-      ],
+      providers: [CampaignDocumentRoleService, PrismaService],
     }).compile()
 
     service = module.get<CampaignDocumentRoleService>(CampaignDocumentRoleService)
+    prismaService = module.get<PrismaService>(PrismaService)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
   })
   it('should return all documents', async () => {
-    const mockList = jest
-      .fn<PrismaPromise<CampaignDocumentRole[]>, []>()
-      .mockResolvedValue(mockData)
-
-    jest.spyOn(prismaService.campaignDocumentRole, 'findMany').mockImplementation(mockList)
-
-    expect(await service.findAll()).toIncludeSameMembers(mockData)
-    expect(await service.findAll()).toHaveLength(3)
-    expect(mockList).toHaveBeenCalled()
-    await expect(service.findAll()).resolves.toEqual(mockData)
+    const findAll = jest.spyOn(prismaService.campaignDocumentRole, 'findMany')
+    const result = await service.findAll()
+    if (result.length) {
+      expect(isUUID(result[0].id)).toBe(true)
+    }
+    expect(result).toBeArray()
+    expect(findAll).toHaveBeenCalled()
   })
   describe('Find one', () => {
-    const id = '5f9221cf-f067-4011-8d2a-66b59509fb7b'
+    let id = ''
+    const data = {
+      name: 'todo',
+      description: 'Bootcamp1',
+    }
     it('should return searching document', async () => {
       const findFirst = jest.spyOn(service, 'findOne')
-      await service.findOne(id)
+      //create
+      const searching = await service.create(data)
+      id = searching.id
+      //findOne
+      const result = await service.findOne(id)
+      //assert
       expect(findFirst).toHaveBeenCalledWith(id)
       expect(isUUID(id)).toBe(true)
+      expect(result).toStrictEqual(searching)
     })
     it('should throw error when id not found', async () => {
-      prismaService.campaignDocumentRole.findFirst.mockRejectedValue(
-        new Error('Document not found!'),
-      )
-      await expect(service.findOne(id)).rejects.toThrowWithMessage(
-        NotFoundException,
-        'Document not found!',
-      )
+      await expect(service.findOne(id + 'p')).rejects.toThrow(NotFoundException)
+    })
+    afterAll(async () => {
+      //clean db
+      if (id) {
+        await service.remove(id)
+      }
     })
   })
   describe('Remove one', () => {
-    const id = '5f9221cf-f067-4011-8d2a-66b59509fb7b'
+    let id = ''
+    const data = {
+      name: 'todo',
+      description: 'Bootcamp1',
+    }
     it('should delete first document', async () => {
-      const removeFirs = jest.spyOn(service, 'remove')
-      prismaService.campaignDocumentRole.delete.mockResolvedValue(mockData[0])
-      await service.remove(id)
-      expect(removeFirs).toHaveBeenCalledWith(id)
+      const remove = jest.spyOn(service, 'remove')
+      //create
+      const searching = await service.create(data)
+      id = searching.id
+      //remove
+      const result = await service.remove(id)
+      //assert
+      expect(remove).toHaveBeenCalledWith(id)
       expect(isUUID(id)).toBe(true)
+      expect(result).toStrictEqual(`${data.name} Deleted Succesfully!`)
     })
     it('should throw error when id not found', async () => {
-      prismaService.campaignDocumentRole.delete.mockRejectedValue(new Error('Document not found!'))
-      await expect(service.remove(id)).rejects.toThrowWithMessage(
-        NotFoundException,
-        'Document not found!',
-      )
+      await expect(service.remove(id + 'p')).rejects.toThrow(NotFoundException)
     })
   })
   describe('Remove many', () => {
+    let id = ''
+    const data = {
+      name: 'todo',
+      description: 'Bootcamp1',
+    }
     const idsToDell = [
       '238facc8-b02c-4958-9805-1df7f05b7e8a',
       '31b72876-f74a-4f38-bcda-3a4db0879871',
@@ -107,67 +93,95 @@ describe('CampaignDocumentRoleService', () => {
       'bd5e7177-ff03-470c-aecb-ac898dca85e3',
       '368698e6-c3cc-4423-9bce-c3cb50d0b2d2',
     ]
-    beforeEach(async () => {
-      const mockList = jest
-        .fn<PrismaPromise<CampaignDocumentRole[]>, []>()
-        .mockResolvedValue(mockData)
-      jest.spyOn(prismaService.campaignDocumentRole, 'findMany').mockImplementation(mockList)
-    })
     it('should throw error when not found ids to remove', async () => {
-      prismaService.campaignDocumentRole.deleteMany.mockRejectedValue(
-        new Error('Document`s ids not found!'),
-      )
-
       await expect(service.removeMany(idsToDell as [string])).rejects.toThrowWithMessage(
         NotFoundException,
-        'Document`s ids not found!',
+        'Requested document`s ids does not exist!',
       )
     })
     it('should remove selected ids', async () => {
       const removeSelected = jest.spyOn(service, 'removeMany')
-      await service.removeMany(idsToDell as [string])
+      //create document
+      const searching = await service.create(data)
+      //push id to list ids
+      idsToDell.push(searching.id)
+      //delete many
+      const removeMany = await service.removeMany(idsToDell as [string])
+      //assert
       expect(removeSelected).toHaveBeenCalledWith(idsToDell)
-      expect(idsToDell.map((id) => isUUID(id))).toEqual([true, true, true, true, true, true, true])
+      expect(idsToDell.map((id) => isUUID(id))).toEqual([
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+      ])
+      expect(removeMany).toStrictEqual(`Deleted Succesfully 1 from 8 tasks!`)
+    })
+    afterAll(async () => {
+      //clean db
+      if (id) {
+        await service.remove(id)
+      }
     })
   })
   describe('Update', () => {
+    let id = ''
     const data = {
-      id: '1ccfac85-1cbd-445d-9619-78cbd6567a44',
-      createdAt: '2022-03-14T09:42:39.259Z',
-      updatedAt: '2022-03-14T09:42:39.260Z',
       name: 'todo',
       description: 'gdgf',
     }
-    const id = '1ccfac85-1cbd-445d-9619-78cbd6567a44'
     it('should udate document', async () => {
       const update = jest.spyOn(service, 'update')
-      await service.update(id, data)
-      expect(update).toHaveBeenCalledWith(id, data)
-      expect(isUUID(data.id)).toEqual(true)
+      //create
+      const searching = await service.create(data)
+      id = searching.id
+      //update
+      const newData = {
+        id,
+        name: 'Todo',
+        description: 'somthing',
+      }
+      const result = await service.update(id, newData)
+      //assert
+      expect(update).toHaveBeenCalledWith(id, newData)
+      expect(isUUID(id)).toEqual(true)
+      expect(result.name).toStrictEqual(newData.name)
+      expect(result.description).toStrictEqual(newData.description)
     })
     it('sholud throw error when id not found', async () => {
-      prismaService.campaignDocumentRole.update.mockRejectedValue(new Error('Document not found!'))
-      await expect(service.update(id, data)).rejects.toThrowWithMessage(
-        NotFoundException,
-        'Document not found!',
-      )
+      await expect(service.update(id + '6', data)).rejects.toThrow(NotFoundException)
+    })
+    afterAll(async () => {
+      //clean db
+      if (id) {
+        await service.remove(id)
+      }
     })
   })
   describe('Create', () => {
+    let id = ''
     const data = {
-      createdAt: '2022-03-14T09:42:39.259Z',
-      updatedAt: '2022-03-14T09:42:39.260Z',
-      name: '',
+      name: 'Bobby',
       description: 'gdgf',
     }
     it('should create document', async () => {
       const create = jest.spyOn(service, 'create')
-      await service.create(data)
+      const result = await service.create(data)
+      id = result.id
       expect(create).toHaveBeenCalledWith(data)
+      expect(isUUID(result.id)).toBe(true)
+      expect(result.name).toStrictEqual(data.name)
+      expect(result.description).toStrictEqual(data.description)
     })
-    it('should throw error required empty field', async () => {
-      prismaService.campaignDocumentRole.create.mockRejectedValue(new Error('Empty field'))
-      await expect(service.create(data)).rejects.toThrowWithMessage(Error, 'Empty field')
+    afterAll(async () => {
+      //clean db
+      if (id) {
+        await service.remove(id)
+      }
     })
   })
 })
