79c6288a7b7689098a77a34ec1553a1ae943a7f5
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-03-24 15:15:58 +0200

    campaignTypeCategory added

--- apps/api/src/domain/generated/campaignType/dto/create-campaignType.dto.ts
+++ apps/api/src/domain/generated/campaignType/dto/create-campaignType.dto.ts
@@ -1,4 +1,9 @@
+import { CampaignTypeCategory } from '@prisma/client'
+import { ApiProperty } from '@nestjs/swagger'
+
 export class CreateCampaignTypeDto {
+  @ApiProperty({ enum: CampaignTypeCategory })
+  category: CampaignTypeCategory
   name: string
   slug: string
   description?: string
