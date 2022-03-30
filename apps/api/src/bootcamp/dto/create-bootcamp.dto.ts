fe294dc059bedf5e5cece1abd1a54817a857c104
Kalin <kalink.wow@gmail.com>
2022-03-30 21:43:47 +0300

    configure create-bootcamp dto

--- apps/api/src/bootcamp/dto/create-bootcamp.dto.ts
+++ apps/api/src/bootcamp/dto/create-bootcamp.dto.ts
@@ -1,1 +1,30 @@
-export class CreateBootcampDto {}
+import { ApiProperty } from '@nestjs/swagger'
+import { IsString } from 'class-validator'
+import { Expose } from 'class-transformer'
+import { Prisma } from '@prisma/client'
+
+@Expose()
+export class CreateBootcampDto {
+  @ApiProperty()
+  @Expose()
+  @IsString()
+  firstName: string
+
+  @ApiProperty()
+  @Expose()
+  @IsString()
+  lastName: string
+
+  @ApiProperty()
+  @Expose()
+  @IsString()
+  city: string
+
+  public toEntity(): Prisma.BootcampCreateInput {
+    return {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      city: this.city,
+    }
+  }
+}
