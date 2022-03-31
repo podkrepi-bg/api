dbbb8a47d5db897d4ca7486fa491d0e195010c07
borislavstoychev <stoy4ew@gmail.com>
2022-03-31 17:32:59 +0300

    :hammer: tests

--- apps/api/src/campaign-document-role/campaign-document-role.controller.spec.ts
+++ apps/api/src/campaign-document-role/campaign-document-role.controller.spec.ts
@@ -1,60 +1,18 @@
+import { NotFoundException } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
+import { isUUID } from 'class-validator'
 import { PrismaService } from '../prisma/prisma.service'
 import { CampaignDocumentRoleController } from './campaign-document-role.controller'
 import { CampaignDocumentRoleService } from './campaign-document-role.service'
 
-const mockData = [
-  {
-    id: '5f9221cf-f067-4011-8d2a-66b59509fb7b',
-    name: 'Bobby',
-    description: '',
-  },
-  {
-    id: 'dc7c8d51-732a-4b82-a87e-f62a9c2e9a68',
-    name: 'image',
-    description: '',
-  },
-  {
-    id: 'dc7c8d51-732a-4b82-a87e-f62a9c2e9a99',
-    name: 'image',
-    description: '',
-  },
-]
-
 describe('CampaignDocumentRoleController', () => {
   let controller: CampaignDocumentRoleController
-
-  const mockCampaignDocumentRoleService = {
-    create: jest.fn((dto) => {
-      return {
-        id: Date.now(),
-        ...dto,
-      }
-    }),
-    findAll: jest.fn().mockReturnValueOnce(mockData),
-    update: jest.fn((id, dto) => ({
-      id,
-      ...dto,
-    })),
-    findOne: jest.fn((id) => {
-      return mockData.find((res) => res.id === id)
-    }),
-    remove: jest.fn((id) => {
-      return mockData.filter((task) => task.id !== id)
-    }),
-    removeMany: jest.fn((ids) => {
-      return mockData.filter((task) => !ids.includes(task.id))
-    }),
-  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CampaignDocumentRoleController],
       providers: [CampaignDocumentRoleService, PrismaService],
-    })
-      .overrideProvider(CampaignDocumentRoleService)
-      .useValue(mockCampaignDocumentRoleService)
-      .compile()
+    }).compile()
 
     controller = module.get<CampaignDocumentRoleController>(CampaignDocumentRoleController)
   })
@@ -62,62 +20,159 @@ describe('CampaignDocumentRoleController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined()
   })
-  it('should create document', () => {
-    const result = controller.create({
-      name: 'todo',
-      description: 'Opravi si zadachata',
-    })
-    const expected = {
-      id: expect.any(Number),
-      name: 'todo',
-      description: 'Opravi si zadachata',
+  it('should return all documents', async () => {
+    const findAll = jest.spyOn(controller, 'findAll')
+    const result = await controller.findAll()
+    if (result.length) {
+      expect(isUUID(result[0].id)).toBe(true)
     }
-    expect(result).toEqual(expected)
-    expect(mockCampaignDocumentRoleService.create).toHaveBeenCalled()
+    expect(findAll).toHaveBeenCalled()
+    expect(findAll).toHaveReturnedTimes(1)
   })
-  it('should return all documents', () => {
-    const result = controller.findAll()
-    expect(result).toHaveLength(3)
-    expect(result).toEqual(mockData)
-    expect(mockCampaignDocumentRoleService.findAll).toHaveBeenCalled()
-  })
-  it('should update a document', () => {
-    const dto = {
+  describe('Find one', () => {
+    let id = ''
+    const data = {
       name: 'todo',
-      description: 'Opravi si zadachata',
+      description: 'Bootcamp1',
     }
-    expect(controller.update('1', dto)).toEqual({
-      id: '1',
-      ...dto,
+    it('should return searching document', async () => {
+      const find = jest.spyOn(controller, 'findOne')
+      //create
+      const searching = await controller.create(data)
+      id = searching.id
+      //findOne
+      const result = await controller.findOne(id)
+      //assert
+      expect(find).toHaveBeenCalledWith(id)
+      expect(find).toHaveReturnedTimes(1)
+      expect(isUUID(id)).toBe(true)
+      expect(result).toStrictEqual(searching)
     })
-    expect(mockCampaignDocumentRoleService.update).toHaveBeenCalledWith('1', dto)
+    it('should throw error when id not found', async () => {
+      await expect(controller.findOne(id + 'p')).rejects.toThrow(NotFoundException)
+    })
+    afterAll(async () => {
+      //clean db
+      if (id) {
+        await controller.remove(id)
+      }
+    })
   })
-  it('should return one document', () => {
-    const result = controller.findOne('5f9221cf-f067-4011-8d2a-66b59509fb7b')
-    const expected = mockData[0]
-    expect(result).toEqual(expected)
-    expect(mockCampaignDocumentRoleService.findOne).toHaveBeenCalledWith(
-      '5f9221cf-f067-4011-8d2a-66b59509fb7b',
-    )
+  describe('Remove', () => {
+    let id = ''
+    const data = {
+      name: 'todo',
+      description: 'Bootcamp1',
+    }
+    it('should delete а document', async () => {
+      const remove = jest.spyOn(controller, 'remove')
+      //create
+      const searching = await controller.create(data)
+      id = searching.id
+      //remove
+      const result = await controller.remove(id)
+      //assert
+      expect(remove).toHaveBeenCalledWith(id)
+      expect(remove).toHaveReturnedTimes(1)
+      expect(isUUID(id)).toBe(true)
+      expect(result).toStrictEqual(`${data.name} Deleted Succesfully!`)
+      await expect(controller.findOne(id)).rejects.toThrow(NotFoundException)
+    })
+    it('should throw error when id not found', async () => {
+      await expect(controller.remove(id)).rejects.toThrow(NotFoundException)
+    })
   })
-  it('should delete one document', () => {
-    const result = controller.remove('5f9221cf-f067-4011-8d2a-66b59509fb7b')
-    expect(result).toHaveLength(2)
-    expect(mockCampaignDocumentRoleService.remove).toHaveBeenCalledWith(
-      '5f9221cf-f067-4011-8d2a-66b59509fb7b',
-    )
-  })
-  it('should delete many documents', () => {
-    const toDell = ['dc7c8d51-732a-4b82-a87e-f62a9c2e9a68', 'dc7c8d51-732a-4b82-a87e-f62a9c2e9a99']
-    const result = controller.removeMany(toDell as [string])
-    const expected = [
-      {
-        id: '5f9221cf-f067-4011-8d2a-66b59509fb7b',
-        name: 'Bobby',
-        description: '',
-      },
+  describe('Remove many', () => {
+    const id = ''
+    const data = {
+      name: 'todo',
+      description: 'Bootcamp1',
+    }
+    const idsToDell = [
+      'd24cc865-7c3d-4862-96a0-8fe9358243e8',
+      'bd5e7177-ff03-470c-aecb-ac898dca85e3',
+      '368698e6-c3cc-4423-9bce-c3cb50d0b2d2',
     ]
-    expect(result).toEqual(expected)
-    expect(mockCampaignDocumentRoleService.removeMany).toHaveBeenCalledWith(toDell)
+    it('should throw error when not found ids to remove', async () => {
+      await expect(controller.removeMany(idsToDell as [string])).rejects.toThrow(NotFoundException)
+    })
+    it('should remove selected ids', async () => {
+      const removeSelected = jest.spyOn(controller, 'removeMany')
+      //create document
+      const searching = await controller.create(data)
+      //push id to list ids
+      idsToDell.push(searching.id)
+      //delete many
+      const removeMany = await controller.removeMany(idsToDell as [string])
+      //assert
+      expect(removeSelected).toHaveBeenCalledWith(idsToDell)
+      expect(removeSelected).toHaveReturnedTimes(1)
+      expect(idsToDell.map((id) => isUUID(id))).toEqual([true, true, true, true])
+      expect(removeMany).toStrictEqual(`Deleted Succesfully 1 from 4 tasks!`)
+    })
+    afterAll(async () => {
+      //clean db
+      if (id) {
+        await controller.remove(id)
+      }
+    })
+  })
+  describe('Update', () => {
+    let id = ''
+    const data = {
+      name: 'todo',
+      description: 'gdgf',
+    }
+    it('should udate document', async () => {
+      const update = jest.spyOn(controller, 'update')
+      //create
+      const searching = await controller.create(data)
+      id = searching.id
+      //update
+      const newData = {
+        id,
+        name: 'Todo',
+        description: 'somthing',
+      }
+      const result = await controller.update(id, newData)
+      //assert
+      expect(update).toHaveBeenCalledWith(id, newData)
+      expect(update).toHaveReturnedTimes(1)
+      expect(isUUID(id)).toEqual(true)
+      expect(result.name).toStrictEqual(newData.name)
+      expect(result.description).toStrictEqual(newData.description)
+    })
+    it('sholud throw error when id not found', async () => {
+      await expect(controller.update(id + '6', data)).rejects.toThrow(NotFoundException)
+    })
+    afterAll(async () => {
+      //clean db
+      if (id) {
+        await controller.remove(id)
+      }
+    })
+  })
+  describe('Create', () => {
+    let id = ''
+    const data = {
+      name: 'Bobby',
+      description: 'gdgf',
+    }
+    it('should create document', async () => {
+      const create = jest.spyOn(controller, 'create')
+      const result = await controller.create(data)
+      id = result.id
+      expect(create).toHaveBeenCalledWith(data)
+      expect(create).toHaveReturnedTimes(1)
+      expect(isUUID(id)).toBe(true)
+      expect(result.name).toStrictEqual(data.name)
+      expect(result.description).toStrictEqual(data.description)
+    })
+    afterAll(async () => {
+      //clean db
+      if (id) {
+        await controller.remove(id)
+      }
+    })
   })
 })
