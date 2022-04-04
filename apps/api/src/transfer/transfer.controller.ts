85774c5daa5a0b7dd6efe091799812ba9e7c2a7c
Ilko Kacharov <kachar136@gmail.com>
2022-04-05 01:32:07 +0300

    Restrict transfers to admins only

--- apps/api/src/transfer/transfer.controller.ts
+++ apps/api/src/transfer/transfer.controller.ts
@@ -1,7 +1,9 @@
+import { RoleMatchingMode, Roles } from 'nest-keycloak-connect'
 import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common'
 
-import { TransferService } from './transfer.service'
+import { RealmViewSupporters, ViewSupporters } from '@podkrepi-bg/podkrepi-types'
 
+import { TransferService } from './transfer.service'
 import { CreateTransferDto } from './dto/create-transfer.dto'
 import { UpdateTransferDto } from './dto/update-transfer.dto'
 
@@ -10,26 +12,46 @@ export class TransferController {
   constructor(private readonly transferService: TransferService) {}
 
   @Post('create')
+  @Roles({
+    roles: [RealmViewSupporters.role, ViewSupporters.role],
+    mode: RoleMatchingMode.ANY,
+  })
   create(@Body() createTransferDto: CreateTransferDto) {
     return this.transferService.create(createTransferDto)
   }
 
   @Get()
+  @Roles({
+    roles: [RealmViewSupporters.role, ViewSupporters.role],
+    mode: RoleMatchingMode.ANY,
+  })
   findAll() {
     return this.transferService.findAll()
   }
 
   @Get(':id')
+  @Roles({
+    roles: [RealmViewSupporters.role, ViewSupporters.role],
+    mode: RoleMatchingMode.ANY,
+  })
   findOne(@Param('id') id: string) {
     return this.transferService.findOne(id)
   }
 
   @Put(':id')
+  @Roles({
+    roles: [RealmViewSupporters.role, ViewSupporters.role],
+    mode: RoleMatchingMode.ANY,
+  })
   update(@Param('id') id: string, @Body() updateTransferDto: UpdateTransferDto) {
     return this.transferService.update(id, updateTransferDto)
   }
 
   @Delete(':id')
+  @Roles({
+    roles: [RealmViewSupporters.role, ViewSupporters.role],
+    mode: RoleMatchingMode.ANY,
+  })
   remove(@Param('id') id: string) {
     return this.transferService.remove(id)
   }
