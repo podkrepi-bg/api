fa86aa3532410f71f6b47d83eb69b8a1fa0d0880
dplamenov <dimitar.plamenov@gmail.com>
2022-04-07 10:20:00 +0300

    return person data

--- apps/api/src/account/account.controller.ts
+++ apps/api/src/account/account.controller.ts
@@ -3,22 +3,25 @@ import { RealmViewSupporters, ViewSupporters } from '@podkrepi-bg/podkrepi-types
 import { AuthenticatedUser, Public, RoleMatchingMode, Roles } from 'nest-keycloak-connect'
 
 import { KeycloakTokenParsed } from '../auth/keycloak'
+import { PersonService } from '../person/person.service'
 import { AccountService } from './account.service'
 
 @Controller('account')
 export class AccountController {
-  constructor(private readonly accountService: AccountService) {}
+  constructor(private readonly accountService: AccountService, private readonly personService: PersonService) {}
 
   @Get('me')
   @Public(false)
-  getMe(
+  async getMe(
     @AuthenticatedUser()
     user: KeycloakTokenParsed,
   ) {
     if (user) {
       // Public authenticated
       console.log(user)
-      return { status: 'authenticated', user }
+      return {
+        status: 'authenticated', user, person: await this.personService.findOne(user.sub as string)
+      }
     }
     // Public
     return { status: 'unauthenticated' }
