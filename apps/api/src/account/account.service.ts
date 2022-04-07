a7abab2a9d1d5af99057f6cf41aea05eb93775a9
dplamenov <dimitar.plamenov@gmail.com>
2022-04-07 16:06:44 +0300

    Update account.service.ts

--- apps/api/src/account/account.service.ts
+++ apps/api/src/account/account.service.ts
@@ -6,6 +6,10 @@ export class AccountService {
     constructor(private prismaService: PrismaService) {}
 
     async updateUserProfile(id: string, data: any) {
+        if (data.birthday) {
+            data.birthday = new Date(data.birthday);
+        }
+
         return await this.prismaService.person.update({where: {id}, data})
     }
 }
