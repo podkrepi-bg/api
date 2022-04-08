e6e082720e5fec87c94b8309a777a7abeeb62f01
dplamenov <dimitar.plamenov@gmail.com>
2022-04-08 10:30:28 +0300

    Update update-person.dto.ts

--- apps/api/src/person/dto/update-person.dto.ts
+++ apps/api/src/person/dto/update-person.dto.ts
@@ -1,14 +1,56 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { Expose } from "class-transformer"
+import { IsString } from "class-validator"
+
 export class UpdatePersonDto {
+  @ApiProperty()
+  @Expose()
+  @IsString()
   firstName?: string
+
+  @ApiProperty()
+  @Expose()
+  @IsString()
   lastName?: string
+
+  @ApiProperty()
+  @Expose()
+  @IsString()
   email?: string
+
+  @ApiProperty()
+  @Expose()
   emailConfirmed?: boolean
+
+  @ApiProperty()
+  @Expose()
   phone?: string
+
+  @ApiProperty()
+  @Expose()
   company?: string
+  
+  @ApiProperty()
+  @Expose()
   newsletter?: boolean
+
+  @ApiProperty()
+  @Expose()
   address?: string
+  
+  @ApiProperty()
+  @Expose()
   birthday?: Date
+  
+  @ApiProperty()
+  @Expose()
   personalNumber?: string
+  
+  @ApiProperty()
+  @Expose()
   keycloakId?: string
+ 
+  @ApiProperty()
+  @Expose()
   stripeCustomerId?: string
 }
