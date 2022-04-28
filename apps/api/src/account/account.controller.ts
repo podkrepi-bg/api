bc0880925ff2915d2e3ed73d5495de0be1020408
Bunny303 <303bunnies@gmail.com>
2022-04-28 17:54:16 +0300

    Added handling of error on create person on registration, fixed getting of authenticated user

--- apps/api/src/account/account.controller.ts
+++ apps/api/src/account/account.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Get, Put } from '@nestjs/common'
+import { Body, Controller, Get, Put, Logger } from '@nestjs/common'
 import { RealmViewSupporters, ViewSupporters } from '@podkrepi-bg/podkrepi-types'
 import { AuthenticatedUser, Public, RoleMatchingMode, Roles } from 'nest-keycloak-connect'
 
 import { KeycloakTokenParsed } from '../auth/keycloak'
+import { Person } from '../domain/generated/person/entities'
 import { UpdatePersonDto } from '../person/dto/update-person.dto'
 import { PersonService } from '../person/person.service'
 import { AccountService } from './account.service'
 
 @Controller('account')
 export class AccountController {
-  constructor(private readonly accountService: AccountService, private readonly personService: PersonService) {}
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly personService: PersonService,
+  ) {}
 
   @Get('me')
   @Public(false)
@@ -18,13 +22,10 @@ export class AccountController {
     user: KeycloakTokenParsed,
   ) {
     if (user) {
-      // Public authenticated
-      console.log(user)
       return {
-        user: await this.personService.findOne(user.sub as string),
+        user: await this.personService.findOneByKeycloakId(user.sub as string),
       }
     }
-    // Public
     return { status: 'unauthenticated' }
   }
 
@@ -38,12 +39,17 @@ export class AccountController {
 
   @Get('new')
   async register(@AuthenticatedUser() user: KeycloakTokenParsed) {
-    const person = await this.personService.create({
-      firstName: user.given_name as string,
-      lastName: user.family_name as string,
-      email: user.email as string,
-      keycloakId: user.sub,
-    })
+    let person: Person | null = null
+    try {
+      person = await this.personService.create({
+        firstName: user.given_name as string,
+        lastName: user.family_name as string,
+        email: user.email as string,
+        keycloakId: user.sub,
+      })
+    } catch (err) {
+      Logger.warn(`Failed to create person for registered user with keycloakId ${user.sub}`)
+    }
 
     return {
       user: person,
