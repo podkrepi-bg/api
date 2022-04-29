25547585e041ef25234ec6fbf0ca9c1d660dfd84
Ilko Kacharov <kachar136@gmail.com>
2022-04-29 16:19:56 +0300

    Added campaign report db migration

---
+++ migrations/20220429131020_campaign_report/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "report_status" AS ENUM ('initial', 'confirmed', 'declined');
+
+-- CreateEnum
+CREATE TYPE "report_reason" AS ENUM ('none', 'duplicate', 'inappropriate', 'illegalActivity', 'misinformation', 'privacyViolation', 'spam', 'irrelevant', 'political', 'discrimination', 'explicitContent', 'fraud', 'other');
+
+-- CreateTable
+CREATE TABLE "campaign_reports" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "campaign_id" UUID NOT NULL,
+    "reported_by_id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+    "status" "report_status" NOT NULL DEFAULT E'initial',
+    "reason" "report_reason" NOT NULL,
+    "report_content" TEXT NOT NULL,
+
+    CONSTRAINT "campaign_reports_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "campaign_reports" ADD CONSTRAINT "campaign_reports_reported_by_id_fkey" FOREIGN KEY ("reported_by_id") REFERENCES "people"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "campaign_reports" ADD CONSTRAINT "campaign_reports_campaign_id_fkey" FOREIGN KEY ("campaign_id") REFERENCES "campaigns"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
