f4718a54ca0ce4c3a956a39282898b409ad977bf
nradkova <neliradkova@abv.bg>
2022-05-11 15:03:46 +0300

    reportFile and s3 services added

--- apps/api/src/info-request/info-request.module.ts
+++ apps/api/src/info-request/info-request.module.ts
@@ -6,6 +6,8 @@ import { SupportService } from '../support/support.service'
 import { EmailService } from '../email/email.service'
 import { TemplateService } from '../email/template.service'
 import { ConfigService } from '@nestjs/config'
+import { ReportFileService } from '../report-file/report-file.service'
+import { S3Service } from '../s3/s3.service'
 
 @Module({
   controllers: [InfoRequestController],
@@ -16,6 +18,8 @@ import { ConfigService } from '@nestjs/config'
     EmailService,
     TemplateService,
     ConfigService,
+    ReportFileService,
+    S3Service,
   ],
 })
 export class InfoRequestModule {}
--- apps/api/src/support/support.module.ts
+++ apps/api/src/support/support.module.ts
@@ -3,11 +3,20 @@ import { EmailService } from '../email/email.service'
 import { TemplateService } from '../email/template.service'
 
 import { PrismaService } from '../prisma/prisma.service'
+import { ReportFileService } from '../report-file/report-file.service'
+import { S3Service } from '../s3/s3.service'
 import { SupportController } from './support.controller'
 import { SupportService } from './support.service'
 
 @Module({
   controllers: [SupportController],
-  providers: [SupportService, PrismaService, EmailService, TemplateService],
+  providers: [
+    SupportService,
+    PrismaService,
+    EmailService,
+    TemplateService,
+    ReportFileService,
+    S3Service,
+  ],
 })
 export class SupportModule {}
