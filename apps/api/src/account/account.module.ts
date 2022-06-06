86122d57380c8802e92f075de9920224abc6589b
nradkova <neliradkova@abv.bg>
2022-06-06 19:14:15 +0300

    AuthModule imported

--- apps/api/src/account/account.module.ts
+++ apps/api/src/account/account.module.ts
@@ -3,8 +3,10 @@ import { PersonService } from '../person/person.service'
 import { PrismaService } from '../prisma/prisma.service'
 import { AccountController } from './account.controller'
 import { AccountService } from './account.service'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
+  imports:[AuthModule],
   controllers: [AccountController],
   providers: [AccountService, PersonService, PrismaService],
 })
