39850bf8967d0eb3df796a4180e697b2d46deae8
GeorgyGenchev <georgygenchev@gmail.com>
2022-06-13 16:53:52 +0300

    update on providers

--- apps/api/src/bank-transactions-file/bank-transactions-file.controller.spec.ts
+++ apps/api/src/bank-transactions-file/bank-transactions-file.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { CampaignService } from '../campaign/campaign.service'
 import { DonationsService } from '../donations/donations.service'
 import { PersonService } from '../person/person.service'
+import { PrismaService } from '../prisma/prisma.service'
+import { S3Service } from '../s3/s3.service'
 import { VaultService } from '../vault/vault.service'
 import { BankTransactionsFileController } from './bank-transactions-file.controller'
 import { BankTransactionsFileService } from './bank-transactions-file.service'
@@ -12,7 +14,15 @@ describe('BankTransactionsFileController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BankTransactionsFileController],
-      providers: [BankTransactionsFileService, DonationsService, VaultService, CampaignService,PersonService],
+      providers: [
+        BankTransactionsFileService,
+        DonationsService,
+        VaultService,
+        CampaignService,
+        PersonService,
+        PrismaService,
+        S3Service
+      ],
     }).compile()
 
     controller = module.get<BankTransactionsFileController>(BankTransactionsFileController)
