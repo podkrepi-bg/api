ccdb95a9aab1da85ec533786903329193ec61716
quantum-grit <quantum-grit@gmail.com>
2022-06-13 09:14:11 +0300

    fixed: migration rename-bankHash-to-paymentRef to ensure existing campaigns are with unique codes

--- migrations/20220605165716_rename_bank_hash_to_payment_reference/migration.sql
+++ migrations/20220605165716_rename_bank_hash_to_payment_reference/migration.sql
@@ -9,5 +9,9 @@ ALTER TABLE "campaigns"
 ALTER TABLE "campaigns" 
   ALTER COLUMN "payment_reference" TYPE varchar(15);
 
+-- make sure all payment-refs are unique taking the middle part of the guid id example: '4052-4A03-9B4C'
+UPDATE "campaigns" SET "payment_reference" = upper(substr(id::text, 10, 14))
+WHERE ("payment_reference" IS NULL OR trim("payment_reference") != '' OR length("payment_reference") < 14);
+
 CREATE UNIQUE INDEX "campaigns_payment_reference_key" ON "campaigns"("payment_reference");
 
