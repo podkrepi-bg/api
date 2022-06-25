ac2b0da5316433900f901ca7fc073c909e502cc6
quantum-grit <quantum-grit@gmail.com>
2022-06-25 14:33:37 +0300

    fixed: uploading of docx type or non-latin file names resulted in errors

---
+++ migrations/20220625111724_extend_file_mimetype_for_storing_full_docx_type/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "campaign_files" ALTER COLUMN "mimetype" SET DATA TYPE VARCHAR(100);
+
+-- AlterTable
+ALTER TABLE "irregularity_files" ALTER COLUMN "mimetype" SET DATA TYPE VARCHAR(100);
