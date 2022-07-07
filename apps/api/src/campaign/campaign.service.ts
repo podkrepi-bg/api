3fa5e617366a4c1fd828da7d11af8bd6d44b9e4f
nradkova <neliradkova@abv.bg>
2022-07-07 10:00:27 +0300

    included: organizer in campaign response data

--- apps/api/src/campaign/campaign.service.ts
+++ apps/api/src/campaign/campaign.service.ts
@@ -43,6 +43,7 @@ export class CampaignService {
         campaignType: { select: { category: true } },
         beneficiary: { select: { person: true } },
         coordinator: { select: { person: true } },
+        organizer: { select: { person: true } },
         vaults: {
           select: {
             donations: { select: { amount: true } },
@@ -65,6 +66,7 @@ export class CampaignService {
         campaignType: { select: { name: true } },
         beneficiary: { select: { person: { select: { firstName: true, lastName: true } } } },
         coordinator: { select: { person: { select: { firstName: true, lastName: true } } } },
+        organizer: { select: { person: { select: { firstName: true, lastName: true } } } },
         vaults: { select: { donations: { select: { amount: true } }, amount: true } },
         incomingTransfers: { select: { amount: true } },
         outgoingTransfers: { select: { amount: true } },
@@ -129,6 +131,12 @@ export class CampaignService {
           },
         },
         coordinator: {
+          select: {
+            id: true,
+            person: { select: { id: true, firstName: true, lastName: true } },
+          },
+        },
+        organizer: {
           select: {
             id: true,
             person: { select: { id: true, firstName: true, lastName: true } },
