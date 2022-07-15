4a80fcb64ba1e6605b114c336b4a4fade9d32e3e
quantum-grit <quantum-grit@gmail.com>
2022-07-15 15:16:05 +0300

    fixed tests

--- apps/api/src/donations/donations.controller.spec.ts
+++ apps/api/src/donations/donations.controller.spec.ts
@@ -92,7 +92,7 @@ describe('DonationsController', () => {
     } as Campaign)
 
     await expect(controller.createCheckoutSession(mockSession)).rejects.toThrow(
-      new NotAcceptableException('This campaign cannot accept donations'),
+      new NotAcceptableException('Campaign cannot accept donations in state: complete'),
     )
     expect(prismaMock.campaign.findFirst).toHaveBeenCalled()
     expect(stripeMock.checkout.sessions.create).not.toHaveBeenCalled()
