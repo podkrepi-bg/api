90631694a5b0f955fb06312a0bd4c059b13da058
nradkova <neliradkova@abv.bg>
2022-07-25 17:52:09 +0300

    removed: unused elements

--- apps/api/src/donation-wish/donation-wish.controller.ts
+++ apps/api/src/donation-wish/donation-wish.controller.ts
@@ -10,14 +10,12 @@ export class DonationWishController {
   @Post()
   @Public()
   create(@Body() createDonationWishDto: CreateDonationWishDto) {
-    console.log(createDonationWishDto)
     return this.donationWishService.create(createDonationWishDto)
   }
 
   @Get('list/:campaignId')
   @Public()
   findList(@Param('campaignId') campaignId: string) {
-    console.log(campaignId)
     return this.donationWishService.findWishesByCampaignId(campaignId)
   }
 }
