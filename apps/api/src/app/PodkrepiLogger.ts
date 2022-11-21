3b82a253db7fdffb9369ba99de9bcefa4569ed02
dimitar.nizamov <dimitur2204@gmail.com>
2022-11-21 18:44:00 +0100

    366 - create PodkrepiLogger

--- apps/api/src/app/PodkrepiLogger.ts
+++ apps/api/src/app/PodkrepiLogger.ts
@@ -5,39 +5,24 @@ export class PodkrepiLogger extends ConsoleLogger {
   constructor(logLevels?: LogLevel[]) {
     super('PodkrepiLogger', { logLevels })
   }
-  /**
-   * Write a 'log' level log.
-   */
+
   log(message: string) {
     super.log(message)
   }
 
-  /**
-   * Write an 'error' level log.
-   */
   error(message: string) {
-    // write the message to a file, send it to the database or do anything
     createFile('/', 'api-error.log', message)
     super.error(message)
   }
 
-  /**
-   * Write a 'warn' level log.
-   */
   warn(message: string) {
     super.warn(message)
   }
 
-  /**
-   * Write a 'debug' level log.
-   */
   debug(message: string) {
     super.debug(message)
   }
 
-  /**
-   * Write a 'verbose' level log.
-   */
   verbose(message: string) {
     super.verbose(message)
   }
