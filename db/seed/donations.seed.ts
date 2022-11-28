a9b4c04607c8a764d992c1d2e1f161c293fa1614
Boyan Vushkov <bvushkov@gmail.com>
2022-11-28 17:39:47 +0200

    Seed more donations for campaigns
    Added two new campaings:
    * One completed with 3 donations
    * One active with 25 donations

--- db/seed/donations.seed.ts
+++ db/seed/donations.seed.ts
@@ -4,6 +4,7 @@ import {
   PrismaClient,
   PaymentProvider,
   DonationStatus,
+  DonationType,
 } from '@prisma/client'
 import faker from 'faker'
 const prisma = new PrismaClient()
@@ -15,11 +16,35 @@ export async function donationsSeed() {
   const campaign = await prisma.campaign.findFirst()
   const vault = await prisma.vault.findFirst()
 
+  const completedCampaignVault = await prisma.vault.findFirst({
+    where: {
+      name: {
+        contains: 'completed',
+      },
+    },
+  })
+
+  const heavilyFundedCampaignVault = await prisma.vault.findFirst({
+    where: {
+      name: {
+        contains: 'heavily-funded',
+      },
+    },
+  })
+
   if (!campaign) {
     throw new Error('There are no vaults created yet!')
   }
   if (!vault) {
     throw new Error('There are no vaults created yet!')
+  }
+
+  if (!completedCampaignVault) {
+    throw new Error('There is no vault for the completed campaign')
+  }
+
+  if (!heavilyFundedCampaignVault) {
+    throw new Error('There is no vault for the heavily funded campaign')
   }
 
   const insert = await prisma.donation.createMany({
@@ -64,4 +89,44 @@ export async function donationsSeed() {
     skipDuplicates: true,
   })
   console.log({ insert })
+
+  console.log('Insert 3 donations for the completed campaign')
+  const donationsForCompletedCampaign = await prisma.donation.createMany({
+    data: [...Array(3).keys()].map(() => {
+      return {
+        type: DonationType.donation,
+        amount: parseInt(faker.finance.amount(10000, 20000)),
+        currency: Currency.BGN,
+        personId: person?.id ?? undefined,
+        targetVaultId: completedCampaignVault.id,
+        extCustomerId: 'cus_' + faker.random.alphaNumeric(8),
+        extPaymentIntentId: 'pi_' + faker.random.alphaNumeric(8),
+        extPaymentMethodId: 'pm_' + faker.random.alphaNumeric(8),
+        provider: PaymentProvider.stripe,
+        status: DonationStatus.succeeded,
+      }
+    }),
+    skipDuplicates: true,
+  })
+  console.log({ donationsForCompletedCampaign })
+
+  console.log('Insert 25 donations for the heavily funded campaign')
+  const donationsForHeavilyFundedCampaign = await prisma.donation.createMany({
+    data: [...Array(25).keys()].map(() => {
+      return {
+        type: DonationType.donation,
+        amount: parseInt(faker.finance.amount(100, 1000)),
+        currency: Currency.BGN,
+        personId: person?.id ?? undefined,
+        targetVaultId: heavilyFundedCampaignVault.id,
+        extCustomerId: 'cus_' + faker.random.alphaNumeric(8),
+        extPaymentIntentId: 'pi_' + faker.random.alphaNumeric(8),
+        extPaymentMethodId: 'pm_' + faker.random.alphaNumeric(8),
+        provider: PaymentProvider.stripe,
+        status: DonationStatus.succeeded,
+      }
+    }),
+    skipDuplicates: true,
+  })
+  console.log({ donationsForHeavilyFundedCampaign })
 }
--- db/seed/vault.seed.ts
+++ db/seed/vault.seed.ts
@@ -1,5 +1,5 @@
 import faker from 'faker'
-import { Currency, PrismaClient } from '@prisma/client'
+import { CampaignState, Currency, PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function vaultSeed() {
@@ -7,10 +7,33 @@ export async function vaultSeed() {
 
   const campaign = await prisma.campaign.findFirst()
 
+  const completedCampaign = await prisma.campaign.findFirst({
+    where: {
+      state: CampaignState.complete,
+    },
+  })
+
+  const heavilyFundedCampaign = await prisma.campaign.findFirst({
+    where: {
+      title: {
+        contains: 'heavily-funded',
+      },
+    },
+  })
+
   if (!campaign) {
     throw new Error('There are no campaigns created yet!')
   }
 
+  if (!completedCampaign) {
+    throw new Error('There is no completed campaign created')
+  }
+
+  if (!heavilyFundedCampaign) {
+    throw new Error('There is no heavily funded campaign created')
+  }
+
+  console.log('Insert 20 vaults for the first campaign')
   const insert = await prisma.vault.createMany({
     data: [...Array(20).keys()].map(() => {
       return {
@@ -22,6 +45,27 @@ export async function vaultSeed() {
     }),
     skipDuplicates: true,
   })
-
   console.log({ insert })
+
+  console.log('Insert one vault for the completed campaign')
+  const compltedCampaignVault = await prisma.vault.create({
+    data: {
+      currency: Currency.BGN,
+      amount: 10000,
+      campaignId: completedCampaign.id,
+      name: faker.finance.accountName() + 'completed',
+    },
+  })
+  console.log({ compltedCampaignVault })
+
+  console.log('Insert one vault for the heavily funded campaign')
+  const heavilyFundedCampaignVault = await prisma.vault.create({
+    data: {
+      currency: Currency.BGN,
+      amount: 10000,
+      campaignId: heavilyFundedCampaign.id,
+      name: faker.finance.accountName() + 'heavily-funded',
+    },
+  })
+  console.log({ heavilyFundedCampaignVault })
 }
