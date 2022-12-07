fe5bc49ab5c04c1803ae5aff0afacd5902f30008
Ani <ani_kalpachka888@abv.bg>
2022-12-07 15:25:42 +0200

    Fix Docker issue (#388)
    Co-authored-by: ani-kalpachka <ani.kalpachka888@abv.bg>

--- db/init.d/create-keycloak-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -u
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -e -c "CREATE USER keycloak WITH PASSWORD 'keycloak'"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -e -c "CREATE DATABASE keycloak"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -e -c "GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak"
