326226815295613a6ca98f9861e76ea214fd79f1
Valentin Kirilov <val.kirilov@gmail.com>
2023-01-27 12:59:26 +0200

    Added more factories for the seeds (#411) (#419)
    - split the DonationWishes seeds to separate module
    - introduce factories for the Campaigns, Donations and DonationWishes seeds
    - standartize the out of the seeds command
    - fix issue with Vault when trying to seed multiple times without resetting the db
    - minor refactoring

---
+++ db/seed/campaign/factory.ts
@@ -0,0 +1,31 @@
+import { Factory } from 'fishery'
+import { faker } from '@faker-js/faker'
+
+import { Campaign } from '.prisma/client'
+import { CampaignState, Currency } from '@prisma/client'
+
+import { getPaymentReference } from '../../../apps/api/src/campaign/helpers/payment-reference'
+
+export const campaignFactory = Factory.define<Campaign>(({ associations }) => ({
+  id: faker.datatype.uuid(),
+  state: faker.helpers.arrayElement(Object.values(CampaignState)),
+  title: faker.lorem.sentence(3),
+  slug: faker.lorem.slug(),
+  essence: faker.company.catchPhrase(),
+  description: faker.lorem.paragraphs(1),
+  coordinatorId: associations.coordinatorId || faker.datatype.uuid(),
+  beneficiaryId: associations.beneficiaryId || faker.datatype.uuid(),
+  campaignTypeId: associations.campaignTypeId || faker.datatype.uuid(),
+  organizerId: associations.organizerId ?? null,
+  companyId: associations.companyId ?? null,
+  approvedById: associations.approvedById || null,
+  currency: Currency.BGN,
+  targetAmount: parseInt(faker.finance.amount(2000, 200000)),
+  allowDonationOnComplete: faker.datatype.boolean(),
+  paymentReference: getPaymentReference(),
+  startDate: faker.date.soon(3),
+  endDate: faker.date.soon(60),
+  createdAt: faker.date.past(),
+  updatedAt: faker.date.recent(),
+  deletedAt: null,
+}))
---
+++ db/seed/donation/seed.ts
@@ -0,0 +1,183 @@
+import { PrismaClient, PaymentProvider, DonationStatus, DonationType, Person } from '@prisma/client'
+
+import { donationFactory } from './factory'
+
+const prisma = new PrismaClient()
+
+const SEED_COMPLETED_CAMPAIGN_DONATIONS = 5
+const SEED_HEAVILY_FUNDED_CAMPAIGN_DONATIONS = 25
+
+export async function donationsSeed() {
+  console.log('Donations seed')
+
+  const person = await prisma.person.findFirst()
+  if (!person) {
+    throw new Error('There are no people created yet!')
+  }
+
+  await seedRandomDonations({ person })
+  await seedDonationsForCompletedCampaign({ person })
+  await seedDonationsForHeavilyFundedCampaign({ person })
+}
+
+interface SeedData {
+  person: Person
+}
+
+async function seedRandomDonations({ person }: SeedData) {
+  const vault = await prisma.vault.findFirst()
+  if (!vault) {
+    throw new Error('There are no vaults created yet!')
+  }
+
+  const donationFactoryOptions = {
+    associations: {
+      personId: person.id,
+      targetVaultId: vault.id,
+    },
+  }
+
+  const randomDonationsData = [
+    donationFactory.build(
+      {
+        type: DonationType.donation,
+        provider: PaymentProvider.stripe,
+        status: DonationStatus.succeeded,
+      },
+      donationFactoryOptions,
+    ),
+    donationFactory.build(
+      {
+        type: DonationType.donation,
+        provider: PaymentProvider.stripe,
+        status: DonationStatus.declined,
+      },
+      donationFactoryOptions,
+    ),
+    donationFactory.build(
+      {
+        type: DonationType.donation,
+        provider: PaymentProvider.bank,
+        status: DonationStatus.initial,
+      },
+      donationFactoryOptions,
+    ),
+  ]
+
+  const insertRandomDonations = await prisma.donation.createMany({
+    data: randomDonationsData,
+    skipDuplicates: true,
+  })
+
+  console.log({ insertRandomDonations })
+
+  console.log('{ Updating first campaign vault: %s }', vault.id)
+  await updateVault(vault.id)
+}
+
+async function seedDonationsForCompletedCampaign({ person }: SeedData) {
+  const completedCampaignVault = await prisma.vault.findFirst({
+    where: {
+      name: {
+        contains: 'completed',
+      },
+    },
+  })
+
+  if (!completedCampaignVault) {
+    throw new Error('There is no vault for the completed campaign')
+  }
+
+  const donationFactoryOptions = {
+    associations: {
+      personId: person.id,
+      targetVaultId: completedCampaignVault.id,
+    },
+  }
+
+  const completedCampaignDonationsData = donationFactory.buildList(
+    SEED_COMPLETED_CAMPAIGN_DONATIONS,
+    {
+      type: DonationType.donation,
+      provider: PaymentProvider.stripe,
+      status: DonationStatus.succeeded,
+    },
+    donationFactoryOptions,
+  )
+
+  const insertCompletedCampaignDonations = await prisma.donation.createMany({
+    data: completedCampaignDonationsData,
+    skipDuplicates: true,
+  })
+
+  console.log({ insertCompletedCampaignDonations })
+
+  console.log('{ Updating completed campaign vault: %s }', completedCampaignVault.id)
+  await updateVault(completedCampaignVault.id)
+}
+
+async function seedDonationsForHeavilyFundedCampaign({ person }: SeedData) {
+  const heavilyFundedCampaignVault = await prisma.vault.findFirst({
+    where: {
+      name: {
+        contains: 'heavily-funded',
+      },
+    },
+  })
+
+  if (!heavilyFundedCampaignVault) {
+    throw new Error('There is no vault for the heavily funded campaign')
+  }
+
+  const donationFactoryOptions = {
+    associations: {
+      personId: person.id,
+      targetVaultId: heavilyFundedCampaignVault.id,
+    },
+  }
+
+  const heavilyFundedCampaignDonationsData = donationFactory.buildList(
+    SEED_HEAVILY_FUNDED_CAMPAIGN_DONATIONS,
+    {
+      type: DonationType.donation,
+      provider: PaymentProvider.stripe,
+      status: DonationStatus.succeeded,
+    },
+    donationFactoryOptions,
+  )
+
+  const insertHeavilyFundedCampaignDonations = await prisma.donation.createMany({
+    data: heavilyFundedCampaignDonationsData,
+    skipDuplicates: true,
+  })
+
+  console.log({ insertHeavilyFundedCampaignDonations })
+
+  console.log('{ Updating heavily-funded campaign vault: %s }', heavilyFundedCampaignVault.id)
+  await updateVault(heavilyFundedCampaignVault.id)
+}
+
+/**
+ * Update the vault after the donations have been created
+ * The amount of the vault should represent the sum of the donations in it
+ * @param vaultId
+ */
+async function updateVault(vaultId: string) {
+  const totalDonationsAmount = await prisma.donation.aggregate({
+    _sum: {
+      amount: true,
+    },
+    where: {
+      targetVaultId: vaultId,
+    },
+  })
+
+  await prisma.vault.update({
+    where: {
+      id: vaultId,
+    },
+    data: {
+      amount: totalDonationsAmount._sum.amount || 0,
+    },
+  })
+}
