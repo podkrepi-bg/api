71670b0942572ad07fea90064f1f0529d34c12f0
Andrey <andrey.goranov@mentormate.com>
2023-02-22 17:17:23 +0200

    remove hardcoded values from pool credentials

--- apps/api/src/sockets/notifications/adapter.ts
+++ apps/api/src/sockets/notifications/adapter.ts
@@ -4,15 +4,15 @@ import { createAdapter } from '@socket.io/postgres-adapter'
 import { Pool } from 'pg'
 import { ServerOptions } from 'socket.io'
 
-const { DB_PORT, DB_USER, DB_PASS, DB_HOST, DB_URL } = process.env
+const { DB_PORT, DB_USER, DB_PASS, DB_HOST } = process.env
 
 export class PostgresIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>
 
   async connectToPostgres(): Promise<void> {
     const pool = new Pool({
-      user: DB_USER || 'localhost',
-      host: 'localhost',
+      user: DB_USER,
+      host: DB_HOST,
       database: 'postgres',
       password: DB_PASS || 'postgrespass',
       port: Number(DB_PORT) || 5432,
