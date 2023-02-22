7447297f08c7af40641f5a26605e8f852054a505
Nnachevvv <nnachevv@icloud.com>
2023-02-22 17:45:25 +0200

    Change DateTime to Date

--- apps/api/src/bank-transactions-file/dto/bank-transactions-import-status.dto.ts
+++ apps/api/src/bank-transactions-file/dto/bank-transactions-import-status.dto.ts
@@ -1,5 +1,3 @@
-import { DateTime } from 'aws-sdk/clients/devicefarm'
-
 export enum ImportStatus {
   UNPROCESSED = 'UNPROCESSED',
   SUCCESS = 'SUCCESS',
@@ -12,6 +10,6 @@ export type BankImportResult = {
   message?: string
   amount: number
   currency: string
-  createdAt: DateTime
+  createdAt: Date
   extPaymentIntentId: string
 }
