c526586d48fb15d20b355b4e6b64deec477de744
quantum-grit <quantum-grit@gmail.com>
2023-03-20 17:56:40 +0200

    added type for stripe config

--- apps/api/src/bank-transactions-file/helpers/use-factory-service.ts
+++ apps/api/src/bank-transactions-file/helpers/use-factory-service.ts
@@ -1,22 +1,24 @@
+import { StripeModuleConfig } from '@golevelup/nestjs-stripe'
 import { ConfigService } from '@nestjs/config'
 import { Public } from 'nest-keycloak-connect'
 
 export const useFactoryService = {
-  useFactory: async (config: ConfigService) => ({
-    apiKey: config.get('stripe.secretKey', ''),
-    webhookConfig: {
-      stripeWebhookSecret: config.get('stripe.webhookSecret', ''),
-      requestBodyProperty: 'body',
-      decorators: [
-        /**
-         * Avoid Keycloak @AuthGuard and @RoleGuard on Webhook controller
-         **/
-        Public(),
-      ],
-      stripeSecrets: {
-        connect: config.get('stripe.webhookSecret', ''),
-        account: config.get('stripe.webhookSecret', ''),
+  useFactory: async (config: ConfigService) =>
+    ({
+      apiKey: config.get('stripe.secretKey', ''),
+      webhookConfig: {
+        stripeWebhookSecret: config.get('stripe.webhookSecret', ''),
+        requestBodyProperty: 'body',
+        decorators: [
+          /**
+           * Avoid Keycloak @AuthGuard and @RoleGuard on Webhook controller
+           **/
+          Public(),
+        ],
+        stripeSecrets: {
+          connect: config.get('stripe.webhookSecret', ''),
+          account: config.get('stripe.webhookSecret', ''),
+        },
       },
-    },
-  }),
+    } as StripeModuleConfig),
 }
