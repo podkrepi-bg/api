6214f91dedcfed7c8c6cf45e22ca919c31c41c62
Dimitar Nizamov <dimitur2204@gmail.com>
2023-03-20 17:11:57 +0100

    add a `@Roles` to the `create-payment` endpoint (#469)

--- apps/api/src/donations/donations.controller.ts
+++ apps/api/src/donations/donations.controller.ts
@@ -151,6 +151,10 @@ export class DonationsController {
   }
 
   @Post('create-payment')
+  @Roles({
+    roles: [RealmViewSupporters.role, ViewSupporters.role],
+    mode: RoleMatchingMode.ANY,
+  })
   create(
     @AuthenticatedUser()
     user: KeycloakTokenParsed,
