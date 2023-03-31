ffd3ec08abea895ee9376e030978bfd2f6f444d0
BogoCvetkov <bogomil_cvetkov@abv.bg>
2023-03-31 19:37:56 +0300

    add missing import in test

--- apps/api/src/bank-transactions/bank-transactions.service.spec.ts
+++ apps/api/src/bank-transactions/bank-transactions.service.spec.ts
@@ -1,18 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { BankTransactionsService } from './bank-transactions.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ExportService } from '../export/export.service'
+import { MockPrismaService } from '../prisma/prisma-client.mock'
+import { BankTransactionsService } from './bank-transactions.service'
 
 describe('BankTransactionsService', () => {
-  let service: BankTransactionsService;
+  let service: BankTransactionsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BankTransactionsService],
-    }).compile();
+      providers: [BankTransactionsService, MockPrismaService, ExportService],
+    }).compile()
 
-    service = module.get<BankTransactionsService>(BankTransactionsService);
-  });
+    service = module.get<BankTransactionsService>(BankTransactionsService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
