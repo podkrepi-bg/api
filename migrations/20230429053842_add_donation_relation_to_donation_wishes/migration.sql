98ff7fac5e90ac6302c06cac0200125f20a781b3
Nikolay Nachev <44066540+Nnachevvv@users.noreply.github.com>
2023-05-02 19:49:05 +0300

    Ensure that there are no orphaned donation wishes

--- migrations/20230429053842_add_donation_relation_to_donation_wishes/migration.sql
+++ migrations/20230429053842_add_donation_relation_to_donation_wishes/migration.sql
@@ -1,2 +1,5 @@
+-- Ensure that there are no orphaned donation wishes
+DELETE FROM "donation_wishes" WHERE "donation_id" NOT IN (SELECT "id" FROM "donations");
+
 -- AddForeignKey
 ALTER TABLE "donation_wishes" ADD CONSTRAINT "donation_wishes_donation_id_fkey" FOREIGN KEY ("donation_id") REFERENCES "donations"("id") ON DELETE SET NULL ON UPDATE CASCADE;
