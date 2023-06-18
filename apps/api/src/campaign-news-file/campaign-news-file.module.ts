9cb1d65cf5092d1997a9858382e6f91fa98b57ea
Alexander Petkov <sashko506@gmail.com>
2023-06-18 14:22:36 +0300

    CampaignNewsFileModule: Remove unused imports

--- apps/api/src/campaign-news-file/campaign-news-file.module.ts
+++ apps/api/src/campaign-news-file/campaign-news-file.module.ts
@@ -4,21 +4,14 @@ import { CampaignNewsFileController } from './campaign-news-file.controller'
 import { PrismaService } from '../prisma/prisma.service'
 import { S3Service } from '../s3/s3.service'
 import { PersonService } from '../person/person.service'
-import { CampaignService } from '../campaign/campaign.service'
-import { VaultService } from '../vault/vault.service'
-import { NotificationModule } from '../sockets/notifications/notification.module'
 
 @Module({
-  imports: [NotificationModule],
-
   controllers: [CampaignNewsFileController],
   providers: [
     CampaignNewsFileService,
     PrismaService,
     S3Service,
     PersonService,
-    CampaignService,
-    VaultService,
   ],
 })
 export class CampaignNewsFileModule {}
