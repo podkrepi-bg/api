7a2113b304d36d19a94687cfdc6a1a024779cb79
igoychev <igoychev@gmail.com>
2023-06-27 12:22:24 +0300

    replaced PersonService with import of PersonModule to fix the tests
    when importing external services, they need to be imported through their module

--- apps/api/src/campaign-news/campaign-news.module.ts
+++ apps/api/src/campaign-news/campaign-news.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { CampaignNewsService } from './campaign-news.service'
 import { CampaignNewsController } from './campaign-news.controller'
 import { PrismaService } from '../prisma/prisma.service'
-import { PersonService } from '../person/person.service'
+import { PersonModule } from '../person/person.module'
 
 @Module({
+  imports: [PersonModule],
   controllers: [CampaignNewsController],
-  providers: [CampaignNewsService, PersonService, PrismaService],
+  providers: [CampaignNewsService, PrismaService],
   exports: [CampaignNewsService],
 })
 export class CampaignNewsModule {}
