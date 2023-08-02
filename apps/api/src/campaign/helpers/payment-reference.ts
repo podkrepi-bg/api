f5cc229ba310febfc86065a42f363fe57b42fdc7
Safvan-tsy <safvanmanikulath@gmail.com>
2023-08-02 14:20:31 +0530

    excluded L

--- apps/api/src/campaign/helpers/payment-reference.ts
+++ apps/api/src/campaign/helpers/payment-reference.ts
@@ -1,11 +1,11 @@
-import { customAlphabet as paymentReferenceGenerator } from 'nanoid';
+import { customAlphabet as paymentReferenceGenerator } from 'nanoid'
 
-const alphabet = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ'; // Exclude 0, 1, O, I, L
-const nanoid = paymentReferenceGenerator(alphabet, 12);
+const alphabet = '23456789ABCDEFGHJKMNPQRSTUVWXYZ' // Exclude 0, 1, O, I, L
+const nanoid = paymentReferenceGenerator(alphabet, 12)
 
 export function getPaymentReference(): string {
-  let id: string = nanoid(); //=> "NY5PKVO4DNBZ"
+  let id: string = nanoid() //=> "NY5PKVO4DNBZ"
   //add dashes for readability "NY5P-KVO4-DNBZ"
-  id = id.slice(0, 4) + '-' + id.slice(4, 8) + '-' + id.slice(8, 12);
-  return id;
+  id = id.slice(0, 4) + '-' + id.slice(4, 8) + '-' + id.slice(8, 12)
+  return id
 }
