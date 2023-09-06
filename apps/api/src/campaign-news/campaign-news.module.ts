f270bed147504aca82c06ddefa65dcfa5842f2ec
Bogsana <bogomil_cvetkov@abv.bg>
2023-09-06 21:03:21 +0300

    add admin notification for campaign news

--- apps/api/src/campaign-news/campaign-news.module.ts
+++ apps/api/src/campaign-news/campaign-news.module.ts
@@ -5,11 +5,12 @@ import { PrismaService } from '../prisma/prisma.service'
 import { PersonModule } from '../person/person.module'
 import { MarketingNotificationsModule } from '../notifications/notifications.module'
 import { ConfigService } from '@nestjs/config'
+import { EmailService } from '../email/email.service'
 
 @Module({
   imports: [PersonModule, MarketingNotificationsModule],
   controllers: [CampaignNewsController],
-  providers: [CampaignNewsService, PrismaService, ConfigService],
+  providers: [CampaignNewsService, PrismaService, ConfigService, EmailService],
   exports: [CampaignNewsService],
 })
 export class CampaignNewsModule {}
