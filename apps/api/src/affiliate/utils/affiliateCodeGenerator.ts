7245c78a2f0ff5f97df62470e6a88da23aaf489a
Alexander Petkov <sashko506@gmail.com>
2023-10-24 17:22:31 +0300

    affiliateCodeGenerator: Get the first 8 characters

--- apps/api/src/affiliate/utils/affiliateCodeGenerator.ts
+++ apps/api/src/affiliate/utils/affiliateCodeGenerator.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto'
 
 export function affiliateCodeGenerator(affiliateId: string) {
-  const uniqueHash = crypto.createHash('sha256').update(affiliateId).digest('hex').slice(0, 5)
+  const uniqueHash = crypto.createHash('sha256').update(affiliateId).digest('hex').slice(0, 8)
   return 'af_' + uniqueHash
 }
