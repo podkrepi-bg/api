50238df52342550ec6137598de90cd2628625e23
Alexander Petkov <sashko506@gmail.com>
2023-10-30 18:33:36 +0200

    src/affiliate: Include JWT_SECRET_KEY when generating affiliate code When generating affiliate code using only affiliateId, the affiliate code could easily be leaked, if some API response returns the affiliateId. This change ensures that this doesn't happen

--- apps/api/src/affiliate/utils/affiliateCodeGenerator.ts
+++ apps/api/src/affiliate/utils/affiliateCodeGenerator.ts
@@ -1,6 +1,10 @@
 import crypto from 'crypto'
 
 export function affiliateCodeGenerator(affiliateId: string) {
-  const uniqueHash = crypto.createHash('sha256').update(affiliateId).digest('hex').slice(0, 8)
+  const uniqueHash = crypto
+    .createHash('sha256')
+    .update(affiliateId + process.env.JWT_SECRET_KEY + new Date())
+    .digest('hex')
+    .slice(0, 8)
   return 'af_' + uniqueHash
 }
