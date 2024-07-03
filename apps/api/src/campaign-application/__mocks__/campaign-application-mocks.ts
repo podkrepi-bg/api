5040e93472d3fa0c53740471c3ea4727d3c95577
Martbul <martbul01@gmail.com>
2024-07-03 18:00:44 +0300

    restructured mocking of data in service.spec and controller.spec

---
+++ apps/api/src/campaign-application/__mocks__/campaign-application-mocks.ts
@@ -0,0 +1,87 @@
+import { CampaignApplicationState, CampaignTypeCategory } from '@prisma/client'
+import { CreateCampaignApplicationDto } from '../dto/create-campaign-application.dto'
+
+export const mockNewCampaignApplication = {
+  campaignName: 'Test Campaign',
+  organizerName: 'Test Organizer',
+  organizerEmail: 'testemail@gmail.com',
+  organizerPhone: '123456789',
+  beneficiary: 'Test beneficary',
+  organizerBeneficiaryRel: 'Test organizerBeneficiaryRel',
+  goal: 'Test goal',
+  history: 'Test history',
+  amount: '1000',
+  description: 'Test description',
+  campaignGuarantee: 'Test guarantee',
+  otherFinanceSources: 'Test otherFinanceSources',
+  otherNotes: 'Test otherNotes',
+  category: CampaignTypeCategory.medical,
+}
+
+const dto: CreateCampaignApplicationDto = {
+  ...mockNewCampaignApplication,
+  acceptTermsAndConditions: true,
+  transparencyTermsAccepted: true,
+  personalInformationProcessingAccepted: true,
+  toEntity: new CreateCampaignApplicationDto().toEntity,
+}
+
+export const mockCampaigns = [
+  {
+    id: '1',
+    createdAt: new Date('2022-04-08T06:36:33.661Z'),
+    updatedAt: new Date('2022-04-08T06:36:33.662Z'),
+    description: 'Test description1',
+    organizerId: 'testOrganizerId1',
+    organizerName: 'Test Organizer1',
+    organizerEmail: 'organizer1@example.com',
+    beneficiary: 'test beneficary1',
+    organizerPhone: '123456789',
+    organizerBeneficiaryRel: 'Test Relation1',
+    campaignName: 'Test Campaign1',
+    goal: 'Test Goal1',
+    history: 'test history1',
+    amount: '1000',
+    campaignGuarantee: 'test campaignGuarantee1',
+    otherFinanceSources: 'test otherFinanceSources1',
+    otherNotes: 'test otherNotes1',
+    state: CampaignApplicationState.review,
+    category: CampaignTypeCategory.medical,
+    ticketURL: 'testsodifhso1',
+    archived: false,
+  },
+  {
+    id: '2',
+    createdAt: new Date('2022-04-08T06:36:33.661Z'),
+    updatedAt: new Date('2022-04-08T06:36:33.662Z'),
+    description: 'Test description2',
+    organizerId: 'testOrganizerId2',
+    organizerName: 'Test Organizer2',
+    organizerEmail: 'organizer2@example.com',
+    beneficiary: 'test beneficary2',
+    organizerPhone: '123456789',
+    organizerBeneficiaryRel: 'Test Relation2',
+    campaignName: 'Test Campaign2',
+    goal: 'Test Goal2',
+    history: 'test history2',
+    amount: '1000',
+    campaignGuarantee: 'test campaignGuarantee2',
+    otherFinanceSources: 'test otherFinanceSources2',
+    otherNotes: 'test otherNotes2',
+    state: CampaignApplicationState.review,
+    category: CampaignTypeCategory.medical,
+    ticketURL: 'testsodifhso2',
+    archived: false,
+  },
+]
+
+export const mockCreatedCampaignApplication = {
+  id: 'mockCampaignApplicationId',
+  createdAt: new Date('2022-04-08T06:36:33.661Z'),
+  updatedAt: new Date('2022-04-08T06:36:33.662Z'),
+  ...dto,
+  organizerId: 'mockOrganizerId',
+  state: 'review',
+  ticketURL: null,
+  archived: false,
+}
--- apps/api/src/campaign-application/campaign-application.controller.spec.ts
+++ apps/api/src/campaign-application/campaign-application.controller.spec.ts
@@ -7,6 +7,7 @@ import { CampaignTypeCategory } from '@prisma/client'
 import { KeycloakTokenParsed, isAdmin } from '../auth/keycloak'
 import { ForbiddenException, NotFoundException } from '@nestjs/common'
 import { PersonService } from '../person/person.service'
+import { mockUser, mockUserAdmin } from './../auth/__mocks__'
 
 describe('CampaignApplicationController', () => {
   let controller: CampaignApplicationController
@@ -33,54 +34,6 @@ describe('CampaignApplicationController', () => {
     category: CampaignTypeCategory.medical,
     toEntity: new CreateCampaignApplicationDto().toEntity,
   } as CreateCampaignApplicationDto
-
-  const mockUser = {
-    exp: 1719866987,
-    iat: 1719866687,
-    jti: '607c488f-6e18-4455-8384-161cec4f1940',
-    iss: 'http://localhost:8180/auth/realms/webapp',
-    aud: 'account',
-    sub: '5795ea9e-ac11-436b-b97d-7b03dbd863f2',
-    typ: 'Bearer',
-    azp: 'jwt-headless',
-    session_state: 'def317ff-0043-4509-ade3-926dd155085e',
-    'allowed-origins': ['*'],
-    realm_access: { roles: ['default-roles-webapp', 'offline_access', 'uma_authorization'] },
-    resource_access: {
-      account: { roles: ['manage-account', 'manage-account-links', 'view-profile'] },
-    },
-    scope: 'openid profile email',
-    sid: 'def317ff-0043-4509-ade3-926dd155085e',
-    email_verified: 'true',
-    name: 'asdasd sdfsdfsdfs',
-    preferred_username: 'martbul01@gmail.com',
-    given_name: 'asdasd',
-    family_name: 'sdfsdfsdfs',
-    email: 'martbul01@gmail.com',
-  } as KeycloakTokenParsed
-
-  const mockUserAdmin = {
-    exp: 1719866987,
-    iat: 1719866687,
-    jti: '607c488f-6e18-4455-8384-161cec4f1940',
-    iss: 'http://localhost:8180/auth/realms/webapp',
-    aud: 'account',
-    sub: '5795ea9e-ac11-436b-b97d-7b03dbd863f2',
-    typ: 'Bearer',
-    azp: 'jwt-headless',
-    session_state: 'def317ff-0043-4509-ade3-926dd155085e',
-    'allowed-origins': ['*'],
-    realm_access: { roles: ['default-roles-webapp', 'offline_access', 'uma_authorization'] },
-    resource_access: { account: { roles: ['manage-account', 'account-view-supporters'] } },
-    scope: 'openid profile email',
-    sid: 'def317ff-0043-4509-ade3-926dd155085e',
-    email_verified: 'true',
-    name: 'asdasd sdfsdfsdfs',
-    preferred_username: 'martbul01@gmail.com',
-    given_name: 'asdasd',
-    family_name: 'sdfsdfsdfs',
-    email: 'martbul01@gmail.com',
-  } as KeycloakTokenParsed
 
   beforeEach(async () => {
     service = autoSpy(CampaignApplicationService)
