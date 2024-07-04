383040c245ef5c26cc2e128d75857cadd7809701
Martbul <martbul01@gmail.com>
2024-07-04 15:36:58 +0300

    fix module providers

--- apps/api/src/campaign-application/campaign-application.module.ts
+++ apps/api/src/campaign-application/campaign-application.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 import { CampaignApplicationService } from './campaign-application.service'
 import { CampaignApplicationController } from './campaign-application.controller'
 import { PrismaModule } from '../prisma/prisma.module'
+import { PersonService } from '../person/person.service'
+import { OrganizerService } from '../organizer/organizer.service'
 @Module({
   imports: [PrismaModule],
   controllers: [CampaignApplicationController],
-  providers: [CampaignApplicationService],
+  providers: [CampaignApplicationService,PersonService,OrganizerService],
 })
 export class CampaignApplicationModule {}
