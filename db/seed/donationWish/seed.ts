f344ecd2b964c2ea1dc0229c539d76fb13cda8f7
Aleksandar Petkov <sashko506@gmail.com>
2024-07-17 23:53:06 +0300

    fix(seed.ts): Fix seeding script failing due to unique constraint (#654)

--- db/seed/donationWish/seed.ts
+++ db/seed/donationWish/seed.ts
@@ -10,36 +10,35 @@ export async function donationsWishesSeed() {
     throw new Error('There are no campaigns created yet!')
   }
 
-  const donations = await prisma.donation.findMany()
-  if (!donations) {
+  const donation = await prisma.donation.findFirst()
+  if (!donation) {
     throw new Error('There are no donations created yet!')
   }
 
-  for (const donation of donations) {
-    const person = await prisma.person.findFirst()
-    if (!person) {
-      throw new Error('There are no people created yet!')
-    }
-    const campaign = await prisma.campaign.findFirst({
-      where: {
-        vaults: {
-          some: {
-            id: donation.targetVaultId,
-          },
+  const person = await prisma.person.findFirst()
+  if (!person) {
+    throw new Error('There are no people created yet!')
+  }
+  const campaign = await prisma.campaign.findFirst({
+    where: {
+      vaults: {
+        some: {
+          id: donation.targetVaultId,
         },
       },
-    })
-    if (!campaign) {
-      throw new Error('There are no campaign with this vault created yet!')
-    }
-    const donationWishData = donationWishFactory.build({
-      campaignId: campaign.id,
-      personId: person.id,
-      donationId: donation.id,
-    })
-
-    await prisma.donationWish.create({
-      data: donationWishData,
-    })
+    },
+  })
+  if (!campaign) {
+    throw new Error('There are no campaign with this vault created yet!')
   }
+  const donationWishData = donationWishFactory.build({
+    campaignId: campaign.id,
+    personId: person.id,
+    donationId: donation.id,
+  })
+
+  await prisma.donationWish.createMany({
+    data: [donationWishData],
+    skipDuplicates: true,
+  })
 }
