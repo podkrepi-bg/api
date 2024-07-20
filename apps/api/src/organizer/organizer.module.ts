e175eedd6882b0092d337b89d2df3c831b266205
Aleksandar <aleksandar.petkov@lab08.com>
2024-07-20 11:37:16 +0300

    fix(OrganizerModule): Export OrganizerService

--- apps/api/src/organizer/organizer.module.ts
+++ apps/api/src/organizer/organizer.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from '../prisma/prisma.module'
   imports: [PrismaModule],
   controllers: [OrganizerController],
   providers: [OrganizerService],
+  exports: [OrganizerService],
 })
 export class OrganizerModule {}
