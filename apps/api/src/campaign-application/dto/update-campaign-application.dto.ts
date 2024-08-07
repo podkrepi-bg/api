2e685618ee589bbd4f523dd302e8ba4a3aa5c264
Martin Kovachki <99181339+Martbul@users.noreply.github.com>
2024-08-07 22:11:15 +0300

    Edit campaing application task(Admin and Organaizer) (#660)
    * working on edit
    
    * add edit campaignApplication
    
    * admin and orgganizer update campaign application
    
    * update max file upload and change file upload on update campaignApplication
    
    * fix file upload
    
    add new endpoint for uploading files to campaignApplication
    remove upload functionality on create and on update campaignApplication

--- apps/api/src/campaign-application/dto/update-campaign-application.dto.ts
+++ apps/api/src/campaign-application/dto/update-campaign-application.dto.ts
@@ -1,4 +1,25 @@
-import { PartialType } from '@nestjs/swagger'
+import { ApiProperty, PartialType } from '@nestjs/swagger'
 import { CreateCampaignApplicationDto } from './create-campaign-application.dto'
+import { Expose } from 'class-transformer'
+import { IsBoolean, IsOptional, IsString } from 'class-validator'
+import { CampaignApplicationState } from '@prisma/client'
 
-export class UpdateCampaignApplicationDto extends PartialType(CreateCampaignApplicationDto) {}
+export class UpdateCampaignApplicationDto extends PartialType(CreateCampaignApplicationDto) {
+  @ApiProperty()
+  @Expose()
+  @IsString()
+  @IsOptional()
+  state?: CampaignApplicationState
+
+  @ApiProperty()
+  @Expose()
+  @IsString()
+  @IsOptional()
+  ticketURL?: string
+
+  @ApiProperty()
+  @Expose()
+  @IsBoolean()
+  @IsOptional()
+  archived?: boolean
+}
