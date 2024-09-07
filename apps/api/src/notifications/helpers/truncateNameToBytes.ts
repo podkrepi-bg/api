90535ae4583ba4dc62425ab5a8a6464cd549cf4c
Aleksandar <aleksandar.petkov@lab08.com>
2024-09-07 17:31:39 +0300

    fix: Lint error

--- apps/api/src/notifications/helpers/truncateNameToBytes.ts
+++ apps/api/src/notifications/helpers/truncateNameToBytes.ts
@@ -2,18 +2,20 @@ export function truncateNameToBytes(name: string, maxBytes: number) {
   const encoder = new TextEncoder()
   const decoder = new TextDecoder()
 
-  let encoded = encoder.encode(name)
+  const encoded = encoder.encode(name)
   if (encoded.length <= maxBytes) {
     return name
   }
 
   let truncated = encoded.slice(0, maxBytes)
 
-  while (true) {
+  for (let i = maxBytes; i > 0; i--) {
     try {
       return decoder.decode(truncated)
     } catch (e) {
       truncated = truncated.slice(0, -1)
     }
   }
+
+  return ''
 }
