de2270d292505ae9c2a8de31cf1c9456255397ed
Aleksandar Petkov <sashko506@gmail.com>
2025-07-07 22:34:00 +0300

    fix: Statistics with integer out of range (#694)

--- apps/api/src/statistics/statistics.service.ts
+++ apps/api/src/statistics/statistics.service.ts
@@ -32,18 +32,22 @@ export class StatisticsService {
         : Prisma.sql`GROUP BY DATE_TRUNC('DAY', d.created_at)`
 
     return this.prisma.$queryRaw`
-    SELECT SUM(d.amount)::INTEGER, COUNT(d.id)::INTEGER, ${date}
-    FROM api.donations d, api.payments p WHERE p.status::text = 'succeeded'
-    ${Prisma.sql`AND target_vault_id IN ( SELECT id from api.vaults WHERE campaign_id = ${campaignId}::uuid)`}
+    SELECT SUM(d.amount)::BIGINT as sum, COUNT(d.id)::INTEGER as count, ${date}
+    FROM api.donations d
+    INNER JOIN api.payments p ON p.id = d.payment_id
+    WHERE p.status::text = 'succeeded'
+    ${Prisma.sql`AND d.target_vault_id IN ( SELECT id from api.vaults WHERE campaign_id = ${campaignId}::uuid)`}
     ${group}
     ORDER BY date ASC `
   }
 
   async listUniqueDonations(campaignId: string): Promise<UniqueDonationsDto[]> {
     return this.prisma.$queryRaw`
-    SELECT d.amount::INTEGER, COUNT(d.id)::INTEGER AS count
-    FROM api.donations d, api.payments p WHERE p.status::text = 'succeeded'
-    ${Prisma.sql`AND target_vault_id IN ( SELECT id from api.vaults WHERE campaign_id = ${campaignId}::uuid)`}
+    SELECT d.amount::INTEGER as amount, COUNT(d.id)::INTEGER AS count
+    FROM api.donations d
+    INNER JOIN api.payments p ON p.id = d.payment_id
+    WHERE p.status::text = 'succeeded'
+    ${Prisma.sql`AND d.target_vault_id IN ( SELECT id from api.vaults WHERE campaign_id = ${campaignId}::uuid)`}
     GROUP BY d.amount
     ORDER BY amount ASC`
   }
@@ -51,8 +55,10 @@ export class StatisticsService {
   async listHourlyDonations(campaignId: string): Promise<HourlyDonationsDto[]> {
     return this.prisma.$queryRaw`
     SELECT EXTRACT(HOUR from d.created_at)::INTEGER AS hour, COUNT(d.id)::INTEGER AS count
-    FROM api.donations d, api.payments p WHERE p.status::text = 'succeeded'
-    ${Prisma.sql`AND target_vault_id IN ( SELECT id from api.vaults WHERE campaign_id = ${campaignId}::uuid)`}
+    FROM api.donations d
+    INNER JOIN api.payments p ON p.id = d.payment_id
+    WHERE p.status::text = 'succeeded'
+    ${Prisma.sql`AND d.target_vault_id IN ( SELECT id from api.vaults WHERE campaign_id = ${campaignId}::uuid)`}
     GROUP BY hour
     ORDER BY hour ASC`
   }
