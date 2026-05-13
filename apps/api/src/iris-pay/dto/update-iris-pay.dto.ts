40768584142402a95f7be9756febbe8f84b03fea
Aleksandar Petkov <aleksandar.petkov@lab08.com>
2026-05-13 12:32:39 +0300

    chore: Remove redundant dto

--- apps/api/src/iris-pay/dto/update-iris-pay.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger'
-// import { CreateIrisPayDto } from './create-iris-pay.dto';
-
-// export class UpdateIrisPayDto extends PartialType(CreateIrisPayDto) {}
